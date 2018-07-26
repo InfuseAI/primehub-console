8840a48f7bbabe2b7830dbb783081feb879ba117
Siou <abz53378@gmail.com>
2018-07-26 14:55:58 +0800

    build antd

---
+++ client/webpack.config.antd.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const {externals, resolve} = require('./webpack.settings');
+const {theme} = require('./package.json');
+// plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/antd.less',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'antd.js',
+    publicPath: '/'
+  },
+  resolveLoader: {
+    moduleExtensions: ['-loader']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'antd.css'
+    })
+  ],
+  module: {
+    rules: [
+      // .ts, .tsx
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          compilerOptions: {
+            module: 'es2015'
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader',
+          options: {
+            modifyVars: theme
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'https://cdn.canner.io/cms-page/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
