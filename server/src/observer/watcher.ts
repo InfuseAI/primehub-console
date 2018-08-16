1155b31dc47a820aefa82fd221d2e5af5d7c7fe0
william chang <wwwy3y3@gmail.com>
2018-08-16 16:42:19 +0800

    add more logs to watcher

--- server/src/observer/watcher.ts
+++ server/src/observer/watcher.ts
@@ -35,40 +35,47 @@ export default class Watcher<T> {
   public watch(options?: {rewatch?: boolean}) {
     const prefix = this.crd.getPrefix();
     console.log(`Watcher: started watching ${this.resource.getResourcePlural()}...`);
-    this.request = this.resource.watch(async (type, object) => {
-      if (type === 'ADDED') {
-        // check if it's already on keycloak
-        console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} added event`);
-        const role = await this.keycloakAdmin.roles.findOneByName({
-          name: `${prefix}${object.metadata.name}`
-        });
-        if (!role) {
-          // create one
-          await this.crd.createOnKeycloak(
-            this.defaultCreateData(object),
-            object.metadata,
-            object.spec,
-            {
-              kcAdminClient: this.keycloakAdmin,
-              everyoneGroupId: this.everyoneGroupId
-            }
-          );
-          console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} added to keycloak`);
-        } else {
-          console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} role already exist`);
-        }
-      } else if (type === 'DELETED') {
-        // delete the role on keycloak
-        try {
-          await this.keycloakAdmin.roles.delByName({
+    this.request = this.resource.watch((type, object) => {
+      const handler = async () => {
+        if (type === 'ADDED') {
+          // check if it's already on keycloak
+          console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} added event`);
+          const role = await this.keycloakAdmin.roles.findOneByName({
             name: `${prefix}${object.metadata.name}`
           });
-          console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} role deleted`);
-        } catch (e) {
-          console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} cannot be deleted`);
-          console.log(e.stack);
+          if (!role) {
+            // tslint:disable-next-line:max-line-length
+            console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} role not exist. Going to add one`);
+            // create one
+            await this.crd.createOnKeycloak(
+              this.defaultCreateData(object),
+              object.metadata,
+              object.spec,
+              {
+                kcAdminClient: this.keycloakAdmin,
+                everyoneGroupId: this.everyoneGroupId
+              }
+            );
+            console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} added to keycloak`);
+          } else {
+            console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} role already exist`);
+          }
+        } else if (type === 'DELETED') {
+          // delete the role on keycloak
+          try {
+            await this.keycloakAdmin.roles.delByName({
+              name: `${prefix}${object.metadata.name}`
+            });
+            console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} role deleted`);
+          } catch (e) {
+            console.log(`Watcher:${this.resource.getResourcePlural()}: ${object.metadata.name} cannot be deleted`);
+            console.log(e.stack);
+          }
         }
-      }
+      };
+
+      handler()
+        .catch(err => console.log(err.stack));
     }, err => {
       if (err) {
         return console.log(err);
