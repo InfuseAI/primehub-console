9ebe725234aaa0d1f25d386094609ea066910a2a
william chang <wwwy3y3@gmail.com>
2018-11-30 17:32:03 +0800

    add validator

--- client/schema/secret.schema.js
+++ client/schema/secret.schema.js
@@ -1,5 +1,5 @@
 /** @jsx CannerScript */
-import CannerScript from 'canner-script';
+import CannerScript, {Condition} from 'canner-script';
 
 export default () => (
   <array ui="tableRoute"
@@ -15,7 +15,18 @@ export default () => (
       }]
     }}
   >
-    <string keyName="name" title="${name}" />
+    <Condition match={(data, operator) => operator === 'create'} defaultMode="disabled">
+      <string keyName="name" title="${name}"
+        validation={{
+          validator: (value, cb) => {
+            if (!value.match(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$/)) {
+              return cb(`lower case alphanumeric characters, '-' or '.', and must start and end with an alphanumeric character.`);
+            }
+          }
+        }}
+        required
+      />
+    </Condition>
     <string keyName="displayName" title="${displayName}" />
     <string keyName="secret" title="${secret}" ui="textarea" />
   </array>
