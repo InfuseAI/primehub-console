42af3f4247102b8039455b22c06aed24872cd251
Siou <abz53378@gmail.com>
2019-07-10 10:30:11 +0800

    make landing page single

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,7 +11,6 @@ import en_US from 'antd/lib/locale-provider/en_US';
 import 'moment/locale/zh-tw';
 addLocaleData([...en, ...zh])
 import CMSPage from './cms';
-import Landing from './landing';
 import schema from '../schema/index.schema.js';
 import Login from './login';
 import myLocales from './utils/locales';
@@ -81,7 +80,6 @@ ReactDOM.render(
         <React.Fragment>
           <Switch>
             {/* <Route path="/login" component={Login} /> */}
-            <Route path="/landing" component={Landing} />
             <Route path={`${(window as any).APP_PREFIX}cms/:activeKey`} component={CMSPage}/>
             <Redirect to={`${(window as any).APP_PREFIX}cms/${firstKey}`}/>
           </Switch>
