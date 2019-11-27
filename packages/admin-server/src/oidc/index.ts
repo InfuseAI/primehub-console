9666436a7ea2bad94fb6a6e346439bdc34d3a4d5
william chang <wwwy3y3@gmail.com>
2019-11-28 01:07:44 +0800

    catch invalid token error in all middlewares

--- packages/admin-server/src/oidc/index.ts
+++ packages/admin-server/src/oidc/index.ts
@@ -5,7 +5,6 @@ import gravatar from 'gravatar';
 import querystring from 'querystring';
 import Token from './token';
 import { URL } from 'url';
-import { ErrorCodes } from '../errorCodes';
 import * as logger from '../logger';
 import { get } from 'lodash';
 import { createHash } from 'crypto';
@@ -103,7 +102,7 @@ export class OidcCtrl {
       return next();
     } catch (err) {
       // redirect to keycloak
-      if (err.data && err.data.code === ERRORS.FORCE_LOGIN) {
+      if (this.isForceLoginError(err) || this.isInvalidRefreshTokenError(err)) {
         logger.warn({
           component: logger.components.authentication,
           type: 'FORCE_LOGIN',
@@ -156,7 +155,7 @@ export class OidcCtrl {
       return next();
     } catch (err) {
       // redirect to keycloak
-      if (err.data && err.data.code === ERRORS.FORCE_LOGIN) {
+      if (this.isForceLoginError(err) || this.isInvalidRefreshTokenError(err)) {
         logger.warn({
           component: logger.components.authentication,
           type: 'FORCE_LOGIN',
@@ -174,26 +173,27 @@ export class OidcCtrl {
   }
 
   public refreshTokenSet = async (ctx: any) => {
-    const refreshToken = ctx.cookies.get('refreshToken', {signed: true});
-    if (!refreshToken) {
-      throw Boom.forbidden('require token');
-    }
-    const refreshTokenInst = new Token(refreshToken, this.clientId);
     const backUrl = this.buildBackUrl(ctx.header.referer);
     const nonce = this.createNonceFromSecret(ctx);
     const redirectUrl = this.getLoginUrl(nonce, backUrl);
 
-    if (refreshTokenInst.isExpired()) {
-      logger.info({
-        component: logger.components.user,
-        type: 'AUTH',
-        message: 'refresh token is expired. Redirecting user to login page'
-      });
-      return ctx.body = {redirectUrl};
-    }
-
     // get refresh token
     try {
+      const refreshToken = ctx.cookies.get('refreshToken', {signed: true});
+      if (!refreshToken) {
+        throw Boom.forbidden('require token', {code: ERRORS.FORCE_LOGIN});
+      }
+      const refreshTokenInst = new Token(refreshToken, this.clientId);
+
+      if (refreshTokenInst.isExpired()) {
+        logger.info({
+          component: logger.components.user,
+          type: 'AUTH',
+          message: 'refresh token is expired. Redirecting user to login page'
+        });
+        return ctx.body = {redirectUrl};
+      }
+
       const tokenSet = await this.oidcClient.refresh(refreshToken);
       // set refreshToken, accessToken on cookie
       ctx.cookies.set('accessToken', tokenSet.access_token, {signed: true});
@@ -217,18 +217,11 @@ export class OidcCtrl {
         accessToken: tokenSet.access_token
       };
     } catch (err) {
-      /**
-       * Possible errors
-       * invalid_grant for expiration
-       * invalid_grant (Session not active) => for user deleted
-       * invalid_scope (User or client no longer has role permissions for client key: realm-management)
-       *  => for remove user from admin
-       */
-      if (err.error === 'invalid_grant' || err.error === 'invalid_scope') {
+      if (this.isForceLoginError(err) || this.isInvalidRefreshTokenError(err)) {
         logger.warn({
           component: logger.components.user,
           type: 'AUTH',
-          message: 'invalid_grant or invalid_scope error',
+          message: 'force to re-login, or invalid_grant or invalid_scope error',
         });
 
         return ctx.body = {redirectUrl};
@@ -329,6 +322,21 @@ export class OidcCtrl {
     const secret = UUID.v1();
     ctx.cookies.set(NONCE_COOKIE, secret, {signed: true});
     return createHash('sha256').update(secret).digest('hex');
+  }
+
+  private isForceLoginError = (err: any) => {
+    return err && err.data && err.data.code === ERRORS.FORCE_LOGIN;
+  }
+
+  private isInvalidRefreshTokenError = (err: any) => {
+    /**
+     * Possible errors
+     * invalid_grant for expiration
+     * invalid_grant (Session not active) => for user deleted
+     * invalid_scope (User or client no longer has role permissions for client key: realm-management)
+     *  => for remove user from admin
+     */
+    return err && (err.error === 'invalid_grant' || err.error === 'invalid_scope');
   }
 }
 
