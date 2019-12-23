091fdb37410052646bc8cb5263e6314308cf5520
Siou <abz53378@gmail.com>
2019-12-23 13:43:00 +0800

    fix select group

--- packages/client/src/containers/jobCreatePage.tsx
+++ packages/client/src/containers/jobCreatePage.tsx
@@ -45,7 +45,7 @@ const appPrefix = (window as any).APP_PREFIX || '/';
 
 class JobCreatePage extends React.Component<Props, State> {
   state = {
-    selectedGroup: null,
+    selectedGroup: 'groupId',
   };
 
   onChangeGroup = (id: string) => {
@@ -69,7 +69,38 @@ class JobCreatePage extends React.Component<Props, State> {
 
   render() {
     const {selectedGroup} = this.state;
-    const {getGroups, createPhJobResult} = this.props;
+    const {createPhJobResult} = this.props;
+    const getGroups = {
+      me: {
+        groups: [{
+          id: 'groupId',
+          displayName: 'displayName',
+          instanceTypes: [{
+            id: 'instanceTypeId',
+            displayName: 'd',
+            description: 'dd'
+          }],
+          images: [{
+            id: 'imageId',
+            displayName: 'image',
+            description: 'dd'
+          }]
+        }, {
+          id: 'groupId2',
+          displayName: 'displayName',
+          instanceTypes: [{
+            id: 'instanceTypeId',
+            displayName: 'd',
+            description: 'dd'
+          }],
+          images: [{
+            id: 'imageId',
+            displayName: 'image',
+            description: 'dd'
+          }]
+        }],
+      }
+    }
     const group = get(getGroups, 'me.groups', []).find(group => group.id === selectedGroup);
     const instanceTypes = get(group, 'instanceTypes', []);
     const images = get(group, 'images', []);
