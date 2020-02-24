f9c2b1143764caa0fa970ec2e4276886b8ba6b3f
Siou <abz53378@gmail.com>
2020-02-24 18:46:36 +0800

    select tab by route

--- packages/client/src/cms-layouts/buildImageTab.tsx
+++ packages/client/src/cms-layouts/buildImageTab.tsx
@@ -3,21 +3,45 @@ import {Tabs} from 'antd';
 import {Item} from 'canner-helpers';
 const TabPane = Tabs.TabPane;
 
-export default class ImageJobTab extends React.Component {
+type Props = {
+  routerParams: {
+    operator: string;
+    payload: Record<string, any>;
+  },
+  goTo: Function;
+  routes: Array<string>;
+}
+
+export default class ImageJobTab extends React.Component<Props> {
+  onChangeTab = (keyName: string) => {
+    const {goTo, routerParams, routes} = this.props;
+    const payload = routerParams.payload || {};
+    goTo({
+      pathname: routes.join('/'),
+      ...routerParams,
+      payload: {
+        ...payload,
+        tab: keyName
+      }
+    });
+  }
+
   render() {
     const {children, routerParams} = this.props;
+    const payload = routerParams.payload || {};
     return (
-      <Tabs>
+      <Tabs activeKey={payload.tab || 'info'} onChange={this.onChangeTab}>
         {
           children
             .filter(child => {
               if (routerParams.operator === 'create' && child.keyName === 'jobs') {
+                // hide jobs tab in create form
                 return false;
               }
               return true;
             })
             .map((child, i) => (
-            <TabPane key={i} tab={child.title} data-testid={`tabs-${child.keyName}`} >
+            <TabPane key={child.keyName} tab={child.title} data-testid={`tabs-${child.keyName}`} >
               <Item
                 hideTitle={true}
                 filter={node => node.keyName === child.keyName}
