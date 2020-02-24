b05281853094f85ebbee876f081c0458f82978ff
Siou <abz53378@gmail.com>
2020-02-24 18:22:53 +0800

    break line in job.message

---
+++ packages/client/src/components/job/message.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+
+type Props = {
+  text: string;
+  extra?: any;
+}
+
+type State = {
+  hidden: boolean;
+}
+
+function renderTexts(texts: Array<string>) {
+  return (
+    <React.Fragment>
+      {texts.map((text, i) => <div key={i}>{text}</div>)}
+    </React.Fragment>
+  )
+}
+
+export default class Message extends React.Component<Props, State> {
+  showMaxLine = 4;
+  state = {
+    hidden: true
+  }
+
+  showMore = () => {
+    this.setState({hidden: false});
+  }
+
+  showLess = () => {
+    this.setState({hidden: true});
+  }
+
+  render() {
+    const {text, extra} = this.props;
+    const {hidden} = this.state;
+    const texts = text.split('\n');
+    return (
+      <div style={{
+        marginTop: '10px',
+        lineHeight: '22px',
+      }}>
+        {renderTexts(texts.slice(0, this.showMaxLine))}
+        {
+          (texts.length > this.showMaxLine && hidden) && (
+            <a style={{display: 'block'}} href="#" onClick={this.showMore}>
+              {`Show More >>`}
+            </a>
+          )
+        }
+        {
+          (texts.length > this.showMaxLine && !hidden) && (
+            <React.Fragment>
+              {renderTexts(texts.slice(this.showMaxLine))}
+              <a style={{display: 'block'}} href="#" onClick={this.showLess}>
+                {`<< Show Less`}
+              </a>
+            </React.Fragment>
+          )
+        }
+        {extra}
+      </div>
+    )
+  }
+}
