655e9582c95959604d047c780a04d9bb1a9628fd
ericy <ericy@infuseai.io>
2020-05-20 17:15:29 +0800

    add phjob null check
    Signed-off-by: Eric Yang <ericy@infuseai.io>

--- packages/client/src/containers/jobDetail.tsx
+++ packages/client/src/containers/jobDetail.tsx
@@ -34,7 +34,7 @@ const getMessage = error => get(error, 'graphQLErrors.0.extensions.code') === 'N
 class JobDetailContainer extends React.Component<Props> {
   render() {
     const {getPhJob, history, rerunPhJob, cancelPhJob, rerunPhJobResult, cancelPhJobResult} = this.props;
-    if (getPhJob.loading) return null;
+    if (!getPhJob.phJob) return null;
     if (getPhJob.error) {
       return getMessage(getPhJob.error)
     };
