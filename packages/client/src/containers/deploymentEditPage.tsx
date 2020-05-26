6cad5c4f73427e3daf5f079d304917ebc9ad2b4a
Jack Pan <jackpan@infuseai.io>
2020-05-26 10:27:37 +0800

    Fix model deployment didn't show global instance type
    Signed-off-by: Jack Pan <jackpan@infuseai.io>

--- packages/client/src/containers/deploymentEditPage.tsx
+++ packages/client/src/containers/deploymentEditPage.tsx
@@ -101,7 +101,7 @@ class DeploymentCreatePage extends React.Component<Props, State> {
     };
 
     const everyoneGroupId = (window as any).EVERYONE_GROUP_ID;
-    const allGroups = get(getGroups, 'me.groups', []).filter(group => group.enabledDeployment);
+    const allGroups = get(getGroups, 'me.groups', []).filter(group => group.enabledDeployment || group.id === everyoneGroupId);
     const groups = allGroups.filter(group => group.id !== everyoneGroupId);
     const everyoneGroup = allGroups.find(group => group.id === everyoneGroupId);
     const selectedGroup = getPhDeployment.phDeployment.groupId;
