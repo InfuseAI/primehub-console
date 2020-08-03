65df7953f4300ee750ebae4b061f728a7c3c99e1
popcorny <celu@infuseai.io>
2020-08-03 13:32:08 +0800

    Fixes the log does not follow
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/graphql-server/src/ee/controllers/jobLogCtrl.ts
+++ packages/graphql-server/src/ee/controllers/jobLogCtrl.ts
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
-import CrdClientImpl, { client as kubeClient } from '../../crdClient/crdClientImpl';
+import CrdClientImpl, { kubeConfig, client as kubeClient } from '../../crdClient/crdClientImpl';
 import { ParameterizedContext } from 'koa';
-import { Stream, Readable } from 'stream';
+import { Stream } from 'stream';
 import { get } from 'lodash';
 import * as logger from '../../logger';
 import PersistLog from '../../utils/persistLog';
+import request from 'request';
 
 const MODEL = 'model';
 
@@ -127,11 +128,33 @@ export class JobLogCtrl {
   private getStream = (
     namespace: string,
     podName: string,
-    options?: {follow?: number, tailLines?: number, container?: string}): Stream => {
+    options?: {follow?: boolean, tailLines?: number, container?: string}): Stream => {
     const container = get(options, 'container');
     const follow = get(options, 'follow', true);
     const tailLines = get(options, 'tailLines');
-    return this.kubeClient.api.v1.namespaces(namespace)
-      .pods(podName).log.getStream({ qs: { container, follow, tailLines } });
+
+    // Use the 'kubernetes@client-nodes' Log API to tail the log.
+    // Ref: https://github.com/kubernetes-client/javascript/blob/0.11.1/src/log.ts
+    const path = `/api/v1/namespaces/${namespace}/pods/${podName}/log`;
+
+    const cluster = kubeConfig.getCurrentCluster();
+    if (!cluster) {
+        throw new Error('No currently active cluster');
+    }
+    const url = cluster.server + path;
+
+    const requestOptions: request.Options = {
+        method: 'GET',
+        qs: {
+          container,
+          follow,
+          tailLines,
+        },
+        uri: url,
+    };
+
+    kubeConfig.applyToRequest(requestOptions);
+
+    return request(requestOptions);
   }
 }
