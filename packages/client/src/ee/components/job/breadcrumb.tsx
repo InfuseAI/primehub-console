2e3a3fde00ba2324573933f877a77b579564cf90
popcorny <celu@infuseai.io>
2020-08-12 16:01:04 +0800

    Add jobs to new UI
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/client/src/ee/components/job/breadcrumb.tsx
+++ packages/client/src/ee/components/job/breadcrumb.tsx
@@ -25,27 +25,35 @@ function getBreadcrumbName(url: string, jobName: string) {
 class JobBreadcrumb extends React.Component<Props> {
 
   render() {
-    const { location, jobName } = this.props;
-    const pathSnippets = location.pathname.split('/').filter(i => i && i !== appPrefix.replace(/\//g, ''));
-    const extraBreadcrumbItems = pathSnippets.map((_, index) => {
-      const url = `${appPrefix}${pathSnippets.slice(0, index + 1).join('/')}`;
-      return (
-        <Breadcrumb.Item key={url}>
-          {
-            pathSnippets.length === index + 1 ?
-            getBreadcrumbName(url, jobName) :
-            <Link to={url}>{getBreadcrumbName(url, jobName)}</Link>
-          }
-        </Breadcrumb.Item>
-      );
-    });
+    const { match, jobName } = this.props;
+    const params = match.params as any
+
+    const basename = params.groupName ? `${appPrefix}g/${params.groupName}` : `${appPrefix}`;
+
     const breadcrumbItems = [
       <Breadcrumb.Item key="home">
-        <a href={`${appPrefix}`}>
-          <Icon type="home" />
-        </a>
+        {
+          params.groupName ?
+          <Link to={`${basename}`}><Icon type="home" /></Link> :
+          <a href={`${basename}`}><Icon type="home" /></a>
+        }
+
       </Breadcrumb.Item>,
-    ].concat(extraBreadcrumbItems);
+      <Breadcrumb.Item key="job">
+        <Link to={`${basename}/job?page=1`}>Job Submission</Link>
+      </Breadcrumb.Item>
+    ];
+
+    if (params.jobId) {
+      breadcrumbItems.push(<Breadcrumb.Item key="detail">
+        Job: {jobName}
+      </Breadcrumb.Item>)
+    } else if (match.url.endsWith('job/create')) {
+      breadcrumbItems.push(<Breadcrumb.Item key="create">
+        Create Job
+      </Breadcrumb.Item>)
+    }
+
     return (
       <Breadcrumb style={{marginBottom: 8}}>
         {breadcrumbItems}
