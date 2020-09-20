9fe74a66c84eb970552ab6d8a44a8411c824fc8d
Aaron Huang <aaroms9733@gmail.com>
2020-09-21 05:35:45 +0800

    Clear
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/ee/components/shared/resourceMonitor.tsx
+++ packages/client/src/ee/components/shared/resourceMonitor.tsx
@@ -38,52 +38,55 @@ export default class ResrouceMonitor extends React.Component<Props, State> {
     const {
       groupContext,
     } = this.props;
-    return (
-      <>
-          <Card style={{overflow: 'auto'}}>
-            <h3>Group Resource</h3>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Type</th>
-                  <th>Used</th>
-                  <th>Limit</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>CPU</td>
-                  <td>{groupContext.resourceStatus.cpuUsage}</td>
-                  <td>{groupContext.quotaCpu == null ? '∞' : groupContext.quotaCpu}</td>
-                </tr>
-                <tr>
-                  <td>Memory</td>
-                  <td>{groupContext.resourceStatus.memUsage} GB</td>
-                  <td>{groupContext.quotaMemory == null ? '∞' : `${groupContext.quotaMemory} GB`} </td>
-                </tr>
-                <tr>
-                  <td>GPU</td>
-                  <td>{groupContext.resourceStatus.gpuUsage} </td>
-                  <td>{groupContext.quotaGpu == null ? '∞' : groupContext.quotaGpu}</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card>
-          <Card style={{overflow: 'auto'}}>
-            <h3>Datasets</h3>
-            {
-              groupContext.datasets.length ? (
-              <ul>
-                {
-                  groupContext.datasets.map(dataset =>(<li>{dataset.displayName}</li>))
-                }
-              </ul>
-              ) : (
-                <div> No available dataset </div>
-              )
-            }
-          </Card>
-      </>
-    );
+    if (groupContext) {
+      return (
+        <>
+            <Card style={{overflow: 'auto'}}>
+              <h3>Group Resource</h3>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>Used</th>
+                    <th>Limit</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>CPU</td>
+                    <td>{groupContext.resourceStatus.cpuUsage}</td>
+                    <td>{groupContext.quotaCpu == null ? '∞' : groupContext.quotaCpu}</td>
+                  </tr>
+                  <tr>
+                    <td>Memory</td>
+                    <td>{groupContext.resourceStatus.memUsage} GB</td>
+                    <td>{groupContext.quotaMemory == null ? '∞' : `${groupContext.quotaMemory} GB`} </td>
+                  </tr>
+                  <tr>
+                    <td>GPU</td>
+                    <td>{groupContext.resourceStatus.gpuUsage} </td>
+                    <td>{groupContext.quotaGpu == null ? '∞' : groupContext.quotaGpu}</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Card>
+            <Card style={{overflow: 'auto'}}>
+              <h3>Datasets</h3>
+              {
+                groupContext.datasets.length ? (
+                <ul>
+                  {
+                    groupContext.datasets.map(dataset =>(<li>{dataset.displayName}</li>))
+                  }
+                </ul>
+                ) : (
+                  <div> No available dataset </div>
+                )
+              }
+            </Card>
+        </>
+      );
+    }
+    return <Card>Somthing Wrong.</Card>
   }
 }
