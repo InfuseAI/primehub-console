ed61e08f6c885d88f128e094d73d6fddde3f3546
Aaron Huang <aaroms9733@gmail.com>
2020-09-21 05:08:24 +0800

    Extract resource monitor to a component
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/ee/components/job/createForm.tsx
+++ packages/client/src/ee/components/job/createForm.tsx
@@ -4,33 +4,10 @@ import {FormComponentProps} from 'antd/lib/form';
 import {get, startCase} from 'lodash';
 import RecurrenceInput, {RecurrenceType, recurrenceValidator} from 'ee/components/schedule/recurrence';
 import Message from 'components/share/message';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import ResourceMonitor from 'ee/components/shared/resourceMonitor';
 
 const { Option } = Select;
-
-const Table = styled.table`
-  width: 100%;
-  td, th {
-    line-height: 2.5em;
-    padding-left: 5px;
-  }
-
-  th {
-    border-bottom: 2px solid #AAA;
-  }
-
-  td {
-    border-bottom: 1px solid #DDD;
-  }
-
-  tbody > tr:nth-child(odd) {
-    background-color: #FAFAFA;
-  }
-
-  tr > td:first-child {
-    font-weight: bold;
-  }
-`;
 
 type Props = FormComponentProps & {
   groupContext?: any;
@@ -441,49 +418,7 @@ class CreateForm extends React.Component<Props, State> {
             </Form.Item>
           </Col>
           <Col xs="24" sm="8" lg="8">
-            <Card style={{overflow: 'auto'}}>
-              <h3>Group Resource</h3>
-              <Table>
-                <thead>
-                  <tr>
-                    <th>Type</th>
-                    <th>Used</th>
-                    <th>Limit</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>CPU</td>
-                    <td>{groupContext.resourceStatus.cpuUsage}</td>
-                    <td>{groupContext.quotaCpu == null ? '∞' : groupContext.quotaCpu}</td>
-                  </tr>
-                  <tr>
-                    <td>Memory</td>
-                    <td>{groupContext.resourceStatus.memUsage} GB</td>
-                    <td>{groupContext.quotaMemory == null ? '∞' : `${groupContext.quotaMemory} GB`} </td>
-                  </tr>
-                  <tr>
-                    <td>GPU</td>
-                    <td>{groupContext.resourceStatus.gpuUsage} </td>
-                    <td>{groupContext.quotaGpu == null ? '∞' : groupContext.quotaGpu}</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Card>
-            <Card style={{overflow: 'auto'}}>
-              <h3>Datasets</h3>
-              {
-                groupContext.datasets.length ? (
-                <ul>
-                  {
-                    groupContext.datasets.map(dataset =>(<li>{dataset.displayName}</li>))
-                  }
-                </ul>
-                ) : (
-                  <div> No available dataset </div>
-                )
-              }
-            </Card>
+            <ResourceMonitor groupContext={groupContext}/>
           </Col>
         </Row>
       </Form>
