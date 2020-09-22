55c377742a2a17780d192fcdafc1d64ba205accc
Ching Yi, Chan <qrtt1@infuseai.io>
2020-09-22 15:55:16 +0800

    Refine error messages

--- packages/graphql-server/src/k8sResource/k8sError.ts
+++ packages/graphql-server/src/k8sResource/k8sError.ts
@@ -2,9 +2,14 @@ import Boom from 'boom';
 import { get } from 'lodash';
 
 const API_UNAVAILABLE = 'API_UNAVAILABLE';
+const RESOURCE_NOT_FOUND = 'RESOURCE_NOT_FOUND';
 
 export const apiUnavailable = () => {
   return Boom.internal(API_UNAVAILABLE, {code: API_UNAVAILABLE});
+};
+
+export const resourceNotFound = (err: Error) => {
+  return Boom.badRequest(err.message, {code: RESOURCE_NOT_FOUND});
 };
 
 export const isErrorApiUnavailable = (error: any) => {
