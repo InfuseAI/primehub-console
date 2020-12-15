0b6d16fd42ca419f02092add75bf28cef3ea2432
popcorny <celu@infuseai.io>
2020-12-15 17:35:16 +0800

    Add jupterhub log
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/client/src/containers/jupyterhubPage.tsx
+++ packages/client/src/containers/jupyterhubPage.tsx
@@ -2,21 +2,68 @@ import * as React from 'react';
 import {compose} from 'recompose';
 import { withGroupContext, GroupContextComponentProps } from 'context/group';
 import IFrame from 'components/hub/iframe';
+import PageTitle from 'components/pageTitle';
+import Log from 'components/share/log';
+import PageBody from 'components/pageBody';
+import { Breadcrumb, Icon, Tabs } from 'antd';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { appPrefix } from 'utils/env';
+import { withRouter } from 'react-router';
 
-type Props = GroupContextComponentProps;
+type Props = GroupContextComponentProps & RouteComponentProps;
 
 class JupyterhubContainer extends React.Component<Props> {
+  renderBreadCrumb () {
+    const { match } = this.props;
+    const params = match.params as any
+    const basename = params.groupName ? `${appPrefix}g/${params.groupName}` : `${appPrefix}`;
+
+    return (
+      <Breadcrumb style={{marginBottom: 8}}>
+        <Breadcrumb.Item key="home">
+          {
+          params.groupName ?
+          <Link to={`${basename}`}><Icon type="home" /></Link> :
+          <a href={`${basename}`}><Icon type="home" /></a>
+          }
+        </Breadcrumb.Item>
+        <Breadcrumb.Item key="notebook">Notebook</Breadcrumb.Item>
+      </Breadcrumb>
+    );
+  }
+
   render() {
     const {groupContext} = this.props;
 
     return (
-      groupContext && groupContext.name ?
-      <IFrame src={`/hub/primehub/home?group=${groupContext.name}`} /> :
-      <div />
+      <>
+        <PageTitle
+          breadcrumb={this.renderBreadCrumb()}
+          title={"Notebook"}
+        />
+        <PageBody style={{flex: '1 1 0%'}}>
+          <Tabs style={{height: '100%'}}>
+            <Tabs.TabPane key="information" tab="Information">
+              <div style={{height: 'calc(100vh - 310px)'}}>
+                <IFrame src={`/hub/primehub/home?group=${groupContext.name}`}/>
+              </div>
+            </Tabs.TabPane>
+            <Tabs.TabPane
+              key="logs"
+              tab="Logs"
+            >
+              <Log
+                endpoint='/api/logs/jupyterhub'
+              />
+            </Tabs.TabPane>
+          </Tabs>
+        </PageBody>
+      </>
     );
   }
 }
 
 export default compose(
-  withGroupContext
+  withGroupContext,
+  withRouter
 )(JupyterhubContainer)
