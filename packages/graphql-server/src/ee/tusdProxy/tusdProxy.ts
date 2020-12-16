7db3c7ee29b640c1ee5dc6b6888e5104f7870f98
Jack Pan <jackpan@infuseai.io>
2020-12-16 17:14:20 +0800

    Add tusd proxy
    Signed-off-by: Jack Pan <jackpan@infuseai.io>
    Co-authored-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ packages/graphql-server/src/ee/tusdProxy/tusdProxy.ts
@@ -0,0 +1,98 @@
+/**
+ * Dependencies
+ */
+import HttpProxy = require('http-proxy');
+import pathMatch = require('path-match');
+import * as logger from '../../logger';
+
+/**
+ * Constants
+ */
+
+const proxy = HttpProxy.createProxyServer({
+  headers: {
+    'X-Forwarded-Host': 'hub.jackpan1.aws.primehub.io/api/tus',
+    'X-Forwarded-Proto': 'https'
+  }
+});
+const route = pathMatch({
+  // path-to-regexp options
+  sensitive: false,
+  strict: false,
+  end: false
+});
+
+let eventRegistered = false;
+
+/**
+ * Koa Http Proxy Middleware
+ */
+export const TusdProxy = (path, options) => (ctx, next) => {
+  // create a match function
+  const match = route(path);
+  if (!match(ctx.path)) {
+    return next();
+  }
+
+  let opts = {...options};
+  if (typeof options === 'function') {
+    const params = match(ctx.path);
+    opts = options.call(options, params);
+  }
+  // object-rest-spread is still in stage-3
+  // https://github.com/tc39/proposal-object-rest-spread
+  const { logs, rewrite, events } = opts;
+
+  const httpProxyOpts = Object.keys(opts)
+    .filter(n => ['logs', 'rewrite', 'events'].indexOf(n) < 0)
+    .reduce((prev, cur) => {
+      prev[cur] = opts[cur];
+      return prev;
+    }, {});
+
+  return new Promise((resolve, reject) => {
+    ctx.req.oldPath = ctx.req.url;
+
+    if (typeof rewrite === 'function') {
+      ctx.req.url = rewrite(ctx.req.url, ctx);
+    }
+
+    if (logs) {
+      typeof logs === 'function' ? logs(ctx, opts.target) : debug(ctx, opts.target);
+    }
+    if (events && typeof events === 'object' && !eventRegistered) {
+      Object.entries(events).forEach(([event, handler]) => {
+        proxy.on(event, handler);
+      });
+      eventRegistered = true;
+    }
+
+    // Let the promise be solved correctly after the proxy.web.
+    // The solution comes from https://github.com/nodejitsu/node-http-proxy/issues/951#issuecomment-179904134
+    ctx.res.on('close', () => {
+      reject(new Error(`Http response closed while proxying ${ctx.req.oldPath}`));
+    });
+
+    ctx.res.on('finish', () => {
+      resolve();
+    });
+
+    proxy.web(ctx.req, ctx.res, httpProxyOpts, e => {
+      const status = {
+        ECONNREFUSED: 503,
+        ETIMEOUT: 504
+      }[e.code];
+      ctx.status = status || 500;
+      resolve();
+    });
+  });
+};
+
+function debug(ctx, target) {
+  logger.info({
+    date: new Date().toISOString(),
+    method: ctx.req.method,
+    oldPath: ctx.req.oldPath,
+    newPath: new URL(ctx.req.url, target)
+  });
+}
