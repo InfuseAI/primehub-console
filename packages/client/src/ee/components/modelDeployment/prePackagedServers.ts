481b5a6e4fa5d4099696af3e139de62ce893cb8a
Timothy Lee <ctiml@infuseai.io>
2020-12-23 15:55:50 +0800

    Fix typo
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- packages/client/src/ee/components/modelDeployment/prePackagedServers.ts
+++ packages/client/src/ee/components/modelDeployment/prePackagedServers.ts
@@ -1,5 +1,5 @@
 export const PrePackagedServers = [
-  { title: 'TensorFlow3 server', url: 'infuseai/tensorflow2-prepackaged_rest:v0.4.2', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-tensorflow2'},
+  { title: 'TensorFlow2 server', url: 'infuseai/tensorflow2-prepackaged_rest:v0.4.2', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-tensorflow2'},
   { title: 'PyTorch server', url: 'infuseai/pytorch-prepackaged_rest:v0.1.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-pytorch'},
   { title: 'SKLearn server', url: 'seldonio/sklearnserver_rest:1.3.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-sklearn'},
 ];
