cf7b1b5ef0ca1494ba8d12f458dc8ab77d595d37
Aaron Huang <aaroms9733@gmail.com>
2021-01-08 16:46:25 +0800

    Update feature flag check
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/components/sharefiles/uploader.tsx
+++ packages/client/src/components/sharefiles/uploader.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { compose } from 'recompose';
+import queryString from 'querystring';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import { Alert } from 'antd';
 import Uppy from '@uppy/core'
 import Tus from '@uppy/tus'
 import { Dashboard } from '@uppy/react'
@@ -8,10 +12,16 @@ import PageBody from 'components/pageBody';
 import Breadcrumbs from 'components/share/breadcrumb';
 import { withAccessToken, AccessTokenComponentProps } from 'components/helpers/withAccessToken';
 import { withGroupContext, GroupContextComponentProps } from 'context/group';
+import {withRouter, Link} from 'react-router-dom';
+import {RouteComponentProps} from 'react-router';
+import { GET_MY_GROUPS, GroupFragment } from 'containers/list';
+import { get } from 'lodash';
 import '@uppy/core/dist/style.css';
 import '@uppy/dashboard/dist/style.css';
 
-type Props = AccessTokenComponentProps & GroupContextComponentProps;
+type Props = RouteComponentProps & AccessTokenComponentProps & GroupContextComponentProps & {
+  getMyGroups: any;
+};
 
 const breadcrumbs = [
   {
@@ -23,7 +33,7 @@ const breadcrumbs = [
 ];
 
 const ShareFilesUploader = (props: Props) => {
-  const { accessToken, groupContext } = props;
+  const { accessToken, groupContext, getMyGroups } = props;
   const endpoint = `${(window as any).cmsHost}${(window as any).graphqlPrefix}/tus`;
   const dirpath = `groups/${groupContext.name}/upload`;
   const headers = {
@@ -32,6 +42,12 @@ const ShareFilesUploader = (props: Props) => {
   const uppy = Uppy({
     autoProceed: true
   });
+  const everyoneGroupId = (window as any).EVERYONE_GROUP_ID;
+  if (getMyGroups.loading) return null;
+  if (getMyGroups.error) return 'Error';
+
+  const groups = get(getMyGroups, 'me.groups', [])
+    .filter(group => group.id !== everyoneGroupId);
 
   uppy
     .use(Tus, {
@@ -45,6 +61,22 @@ const ShareFilesUploader = (props: Props) => {
       const url = result.successful[0].uploadURL
       console.log('Upload compelete!', url, result.successful);
     });
+  if ( groupContext ) {
+    const group = groups.find(group => group.id === groupContext.id);
+    if ( !group ) {
+      return (<Alert
+        message="Group not found"
+        description={`Group ${groupContext.name} is not found or not authorized.`}
+        type="error"
+        showIcon/>);
+    } else if ( window.enablePhfs !== true ) {
+      return (<Alert
+        message="Feature not available"
+        description="PrimeHub File System is not enabled for selected group. Please contact your administrator to enable this feature."
+        type="warning"
+        showIcon/>);
+    }
+  }
   return (
     <div>
       <PageTitle
@@ -83,5 +115,20 @@ const ShareFilesUploader = (props: Props) => {
 
 export default compose(
   withAccessToken,
-  withGroupContext
+  withGroupContext,
+  withRouter,
+  graphql(GET_MY_GROUPS, {
+    name: 'getMyGroups',
+    options: (props: Props) => ({
+      onCompleted: data => {
+        // default  page=1
+        if (props.location.search) return;
+        props.history.replace({
+          pathname: props.location.pathname,
+          search: queryString.stringify({page: 1})
+        });
+      },
+      fetchPolicy: 'cache-and-network'
+    }),
+  })
 )(ShareFilesUploader);
