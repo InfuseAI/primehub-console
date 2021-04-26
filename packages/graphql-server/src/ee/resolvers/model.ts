99fbc9b97ae17d96f7ba875d3d1647356828ecd9
Timothy Lee <ctiml@infuseai.io>
2021-04-26 11:07:12 +0800

    Handle model and version not found
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- packages/graphql-server/src/ee/resolvers/model.ts
+++ packages/graphql-server/src/ee/resolvers/model.ts
@@ -3,6 +3,9 @@ import { ApolloError } from 'apollo-server';
 import fetch from 'node-fetch';
 import { Context } from '../../resolvers/interface';
 import { toRelay, filter, paginate, extractPagination, isGroupMember } from '../../resolvers/utils';
+import * as logger from '../../logger';
+import { ErrorCodes } from '../../errorCodes';
+const { NOT_AUTH_ERROR, INTERNAL_ERROR } = ErrorCodes;
 
 // mlflow api endpoints
 const API_ENDPOINT_MODEL_LIST = '/api/2.0/preview/mlflow/registered-models/list';
@@ -10,9 +13,6 @@ const API_ENDPOINT_MODEL_GET = '/api/2.0/preview/mlflow/registered-models/get';
 const API_ENDPOINT_MODEL_VERSION_SEARCH = '/api/2.0/preview/mlflow/model-versions/search';
 const API_ENDPOINT_MODEL_VERSION_GET = '/api/2.0/preview/mlflow/model-versions/get';
 const API_ENDPOINT_RUN_GET = '/api/2.0/preview/mlflow/runs/get';
-
-// error const
-const NOT_AUTH_ERROR = 'NOT_AUTH';
 
 const requestApi = async (trackingUri: string, endpoint: string, params = {}) => {
   const url = new URL(`${trackingUri}${endpoint}`);
@@ -89,9 +89,19 @@ export const queryOne = async (root, args, context: Context) => {
     throw new ApolloError('user not auth', NOT_AUTH_ERROR);
   }
 
-  // TODO: handle name not found
   const json = await requestApi(getTrackingUri(where.group), API_ENDPOINT_MODEL_GET, { name: where.name });
-  return transform(json.registered_model);
+  if (json.registered_model) {
+    return transform(json.registered_model);
+  } else if (json.error_code) {
+    logger.error({
+      component: logger.components.model,
+      type: json.error_code,
+      message: json.message,
+    });
+    throw new ApolloError(`model '${where.name}' not found`, json.error_code);
+  } else {
+    throw new ApolloError('failed to get model', INTERNAL_ERROR);
+  }
 };
 
 export const query = async (root, args, context: Context) => {
@@ -103,7 +113,17 @@ export const query = async (root, args, context: Context) => {
   }
 
   const json = await requestApi(getTrackingUri(where.group), API_ENDPOINT_MODEL_LIST);
-  return paginate(json.registered_models.map(m => transform(m)), extractPagination(args));
+  if (json.registered_models) {
+    return paginate(json.registered_models.map(m => transform(m)), extractPagination(args));
+  } else if (json.error_code) {
+    logger.error({
+      component: logger.components.model,
+      type: json.error_code,
+      message: json.message,
+    });
+  } else {
+    return [];
+  }
 };
 
 export const queryVersion = async (root, args, context: Context) => {
@@ -115,10 +135,20 @@ export const queryVersion = async (root, args, context: Context) => {
   }
 
   const trackingUri = getTrackingUri(where.group);
-  // TODO: handle name not found
   const json = await requestApi(trackingUri, API_ENDPOINT_MODEL_VERSION_GET, { name: where.name, version: where.version });
-  json.model_version.run = await getRun(trackingUri, json.model_version.run_id);
-  return transformVersion(json.model_version);
+  if (json.model_version) {
+    json.model_version.run = await getRun(trackingUri, json.model_version.run_id);
+    return transformVersion(json.model_version);
+  } else if (json.error_code) {
+    logger.error({
+      component: logger.components.model,
+      type: json.error_code,
+      message: json.message,
+    });
+    throw new ApolloError(`model '${where.name}' version '${where.version}' not found`, json.error_code);
+  } else {
+    throw new ApolloError('failed to get model version', INTERNAL_ERROR);
+  }
 };
 
 const listQueryVersions = async (where: any, context: Context) => {
@@ -130,13 +160,22 @@ const listQueryVersions = async (where: any, context: Context) => {
 
   const trackingUri = getTrackingUri(where.group);
   const search = `name='${where.name}'`;
-  // TODO: handle name not found
   const json = await requestApi(trackingUri, API_ENDPOINT_MODEL_VERSION_SEARCH, { filter: search });
-  const modelVersions = json.model_versions.map(async m => {
-    m.run = await getRun(trackingUri, m.run_id);
-    return transformVersion(m);
-  });
-  return filter(modelVersions, where);
+  if (json.model_versions) {
+    const modelVersions = json.model_versions.map(async m => {
+      m.run = await getRun(trackingUri, m.run_id);
+      return transformVersion(m);
+    });
+    return filter(modelVersions, where);
+  } else if (json.error_code) {
+    logger.error({
+      component: logger.components.model,
+      type: json.error_code,
+      message: json.message,
+    });
+  } else {
+    return [];
+  }
 };
 
 export const queryVersions = async (root, args, context: Context) => {
