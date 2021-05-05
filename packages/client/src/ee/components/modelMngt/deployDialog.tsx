8be6e6a739659ced0a4d2eee186ed981d626380c
popcorny <celu@infuseai.io>
2021-05-05 15:40:17 +0800

    Handle the onDeploy behavior
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/client/src/ee/components/modelMngt/deployDialog.tsx
+++ packages/client/src/ee/components/modelMngt/deployDialog.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Form, Modal, Select, Table} from 'antd';
+import {find} from 'lodash';
 
-export interface DeployDialogProps {
+interface Props {
   modelVersion;
   deploymentRefs;
   onDeployNew?: (modelVersion: Object) => void;
@@ -9,29 +10,75 @@ export interface DeployDialogProps {
   onCancel?: () => void;
 };
 
-export function DeployDialog(props: DeployDialogProps) {
-  const {modelVersion, deploymentRefs, onDeployNew, onDeployExisting, onCancel} = props;
+interface State {
+  value: string;
+}
 
-  return <Modal
-          title={`Deploy Model`}
-          visible={true}
-          onCancel={onCancel ? () => onCancel() : undefined}
-        >
-          <Form><Form.Item label="Deployment" required={true}>
-          <Select defaultValue="" style={{width: 400}} onChange={()=>{}}>
+export class DeployDialog extends React.Component<Props, State> {
+  state = {
+    value: "",
+  }
 
-            <Select.Option value="">+ Create new deployment</Select.Option>
-            {
-              deploymentRefs.length > 0 ?
+  private handleChange = (value) => {
+    this.setState({value});
+  };
 
-              <Select.OptGroup label="Update existing deployments">
-                {
-                  deploymentRefs.map((item)=> <Select.Option value={item.id}>{item.name}</Select.Option>)
-                }
-              </Select.OptGroup>: <></>
-            }
-          </Select>
-          </Form.Item></Form>
-        </Modal>
+  private handleOk = () => {
+    const {modelVersion, deploymentRefs, onDeployNew, onDeployExisting} = this.props;
+    const {value} = this.state;
+
+    if (value === "") {
+      if (onDeployNew) {
+        onDeployNew(modelVersion);
+      }
+    } else {
+      if (onDeployExisting) {
+        const deploymentRef = find(deploymentRefs, {id: value});
+        onDeployExisting(modelVersion, deploymentRef);
+      }
+    }
+  }
+
+  private handleCancel = () => {
+    const {onCancel} = this.props;
+    if (onCancel) {
+      onCancel();
+    }
+  }
+
+  render () {
+    const {
+      deploymentRefs,
+      onCancel
+    } = this.props;
+
+    const {
+      value,
+    } = this.state;
+
+
+    return <Modal
+            title={`Deploy Model`}
+            visible={true}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <Form><Form.Item label="Deployment" required={true}>
+            <Select defaultValue={value} style={{width: 400}} onChange={this.handleChange}>
+
+              <Select.Option value="">+ Create new deployment</Select.Option>
+              {
+                deploymentRefs.length > 0 ?
+
+                <Select.OptGroup label="Update existing deployments">
+                  {
+                    deploymentRefs.map((item)=> <Select.Option value={item.id}>{item.name}</Select.Option>)
+                  }
+                </Select.OptGroup>: <></>
+              }
+            </Select>
+            </Form.Item></Form>
+          </Modal>;
+  }
 }
 
