b56b6d89bb63dcb686cc656aa9a73d6635701be9
Jie Peng <im@jiepeng.me>
2021-05-18 17:34:18 +0800

    chore(client): add eslint related rules and format webpack configs
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/webpack.config.ce.js
+++ packages/client/webpack.config.ce.js
@@ -1,40 +1,47 @@
-const antdTheme = require('./package.json').theme;
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const tsImportPluginFactory = require('ts-import-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const {externals, resolve} = require('./webpack.settings');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { externals, resolve } = require('./webpack.settings');
 const path = require('path');
 const webpack = require('webpack');
 const devMode = process.env.NODE_ENV !== 'production';
 const CompressionPlugin = require('compression-webpack-plugin');
 
-resolve.alias['index-schema'] = path.resolve(__dirname, 'schema/index.ce.schema.js');
+resolve.alias['index-schema'] = path.resolve(
+  __dirname,
+  'schema/index.ce.schema.js'
+);
 
 module.exports = {
   entry: {
-    cms: devMode ? './src/index.tsx' : ['./src/public-import.js', './src/index.tsx'],
-    'main': devMode ? './src/main.ce.tsx' : ['./src/public-import.js', './src/main.ce.tsx'],
+    cms: devMode
+      ? './src/index.tsx'
+      : ['./src/public-import.js', './src/index.tsx'],
+    main: devMode
+      ? './src/main.ce.tsx'
+      : ['./src/public-import.js', './src/main.ce.tsx'],
   },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: devMode ? 'http://localhost:8090/' : ''
+    publicPath: devMode ? 'http://localhost:8090/' : '',
   },
   mode: devMode ? 'development' : 'production',
   externals,
   resolve,
   devServer: {
-    port: "8090",
+    port: '8090',
     contentBase: path.join(__dirname, 'dist'),
     historyApiFallback: {
       rewrites: [
         { from: /^\/g/, to: '/index.html' },
         { from: /^\/app-prefix\/g/, to: '/index.html' },
-        { from: /./, to: '/cms.html' }
-      ]
+        { from: /./, to: '/cms.html' },
+      ],
     },
-    https: false
+    https: false,
   },
   module: {
     rules: [
@@ -44,15 +51,17 @@ module.exports = {
         options: {
           transpileOnly: true,
           compilerOptions: {
-            module: 'es2015'
+            module: 'es2015',
           },
           getCustomTransformers: () => ({
-            before: [tsImportPluginFactory({
-              libraryName: 'antd',
-              style: true,
-            })]
+            before: [
+              tsImportPluginFactory({
+                libraryName: 'antd',
+                style: true,
+              }),
+            ],
           }),
-        }
+        },
       },
       {
         test: /\.(graphql|gql)$/,
@@ -61,44 +70,51 @@ module.exports = {
       },
       {
         test: /(\.schema\.js|canner\.def\.js)$/,
-        use: [{
-          loader: "canner-schema-loader"
-        }, {
-          loader: "babel-loader"
-        }]
-      }, {
+        use: [
+          {
+            loader: 'canner-schema-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }, {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', "css-loader"]
-      }, {
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.less$/,
-        loader: 'ignore-loader'
-      }, {
+        loader: 'ignore-loader',
+      },
+      {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
-            options: {}
-          }
-        ]
-      }
-    ]
+            options: {},
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       chunks: ['cms'],
       template: 'docs/index.html',
-      filename: 'cms.html'
+      filename: 'cms.html',
     }),
     new HtmlWebPackPlugin({
       chunks: ['main'],
       template: 'docs/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.DefinePlugin({
@@ -111,8 +127,8 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: devMode ? 'server' : 'disabled',
-      openAnalyzer: false
+      openAnalyzer: false,
     }),
-    new CompressionPlugin()
-  ]
+    new CompressionPlugin(),
+  ],
 };
