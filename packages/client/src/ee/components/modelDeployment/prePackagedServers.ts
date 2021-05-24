8f0a20fd9b4f910a681656bb02f3b4c6eb9f6711
Ching Yi, Chan <qrtt1@infuseai.io>
2021-05-24 15:20:57 +0800

    update prepackaged server
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- packages/client/src/ee/components/modelDeployment/prePackagedServers.ts
+++ packages/client/src/ee/components/modelDeployment/prePackagedServers.ts
@@ -1,5 +1,5 @@
 export const PrePackagedServers = [
   { title: 'TensorFlow2 server', url: 'infuseai/tensorflow2-prepackaged:v0.2.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-tensorflow2'},
   { title: 'PyTorch server', url: 'infuseai/pytorch-prepackaged:v0.2.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-pytorch'},
-  { title: 'SKLearn server', url: 'seldonio/sklearnserver_rest:1.3.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-sklearn'},
+  { title: 'SKLearn server', url: 'infuseai/sklearn-prepackaged:v0.1.0', docLink: 'https://docs.primehub.io/docs/model-deployment-prepackaged-server-sklearn'},
 ];
