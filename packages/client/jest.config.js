96f8782fc6902955b6723e62098c00bbc12448f3
Jie Peng <im@jiepeng.me>
2021-06-09 15:06:53 +0800

    chore(test): move mock files into test folder
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   moduleNameMapper: {
     '^test/(.*)': '<rootDir>/test/$1',
     '^utils/(.*)': '<rootDir>/src/utils/$1',
-    '^images/(.*)': '<rootDir>/__mocks__/fileMocks.js',
     '^components/(.*)': '<rootDir>/src/components/$1',
     '^containers/(.*)': '<rootDir>/src/containers/$1',
     '^cms-layouts/(.*)': '<rootDir>/src/cms-layouts/$1',
@@ -16,10 +15,14 @@ module.exports = {
     '^interfaces/(.*)': '<rootDir>/src/interfaces/$1',
     '^queries/(.*)': '<rootDir>/src/queries/$1',
     '^@types/(.*)': '<rootDir>/src/@types/$1',
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMocks.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/fileMocks.js'
+    '^images/(.*)': '<rootDir>/test/__mocks__/fileMocks.js',
+    '\\.(css|less)$': '<rootDir>/test/__mocks__/fileMocks.js',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test__mocks__/fileMocks.js',
   },
+
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+
   testEnvironment: 'jsdom',
   testURL: 'http://localhost/',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
@@ -27,7 +30,6 @@ module.exports = {
   transformIgnorePatterns: ['/node-modules/', '/dist/'],
   transform: {
     '\\.tsx?$': 'ts-jest',
-    // '\\.jsx?$': 'babel-jest',
-    '\\.[jt]sx?$': 'babel-jest',
+    '\\.[j]sx?$': 'babel-jest',
   },
 };
