2f507cb9fa27854c01ab165470bd9e29c2e98c40
Timothy Lee <ctiml@infuseai.io>
2021-06-15 14:57:36 +0800

    Add unittest for group images creation page
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

---
+++ packages/client/src/containers/__tests__/imageCreatePage.spec.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { render, screen } from 'test/test-utils';
+import { MemoryRouter } from 'react-router-dom';
+import ImageCreatePage from 'containers/imageCreatePage';
+import { GroupContext } from 'context/group';
+import { UserContext } from 'context/user';
+import { MockedProvider } from 'react-apollo/test-utils';
+
+const AllTheProviders = ({ children }) => {
+  const groupValue = {
+    id: 'test-group',
+    name: 'test-group',
+    displayName: 'test-group',
+    admins: 'test',
+    enabledSharedVolume: true,
+    enabledDeployment: true,
+  };
+
+  const userValue = {
+    id: '1',
+    username: 'test',
+    isCurrentGroupAdmin: true,
+  };
+
+  return (
+    <MockedProvider>
+      <GroupContext.Provider value={groupValue}>
+        <UserContext.Provider value={userValue}>
+          <MemoryRouter>{children}</MemoryRouter>
+        </UserContext.Provider>
+      </GroupContext.Provider>
+    </MockedProvider>
+  );
+};
+
+describe('ImageCreateForm Container', () => {
+  it('Should render create page properly', async () => {
+    render(<ImageCreatePage />, { wrapper: AllTheProviders });
+    expect(await screen.findByText('Container Image URL')).toBeInTheDocument();
+  });
+});
