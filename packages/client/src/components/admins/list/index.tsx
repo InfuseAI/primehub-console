d8d5d1e2fbf5965f84acd4933ee06c8aa05129fa
Aaron Huang <aaroms9733@gmail.com>
2021-06-25 16:02:09 +0800

    Update layout
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/components/admins/list/index.tsx
+++ packages/client/src/components/admins/list/index.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { filter } from 'lodash';
 import {Table, Layout, Menu, Divider } from 'antd';
-import PageTitle from 'components/pageTitle';
-import PageBody from 'components/pageBody';
 
 interface Props {
   dataSource: Array<any>;
@@ -15,13 +13,13 @@ export function List(props: Props) {
   // Extend the column config, a condition visible config.
   const columns = filter(props.columns, (obj) => obj.visible !== false)
   return (
-    <PageBody>
+    <React.Fragment>
       <Table
         loading={props.loading}
         dataSource={props.dataSource}
         columns={columns}
         rowKey={(record, index) => record.id}
       />
-    </PageBody>
+    </React.Fragment>
   );
 }
