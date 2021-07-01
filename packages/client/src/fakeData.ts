c6a69d273b31cd64461212283e6b6450709621de
Jie Peng <im@jiepeng.me>
2021-07-01 10:53:55 +0800

    chore: add systeem fakeData
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/fakeData.ts
+++ packages/client/src/fakeData.ts
@@ -10,7 +10,8 @@ import models from './fakeData/models';
 import modelVersions from './fakeData/modelVersions';
 import phApplications from './fakeData/phApplications';
 import phAppTemplates from './fakeData/phAppTemplates';
-import {groups, mlflow} from './fakeData/groups';
+import { groups, mlflow } from './fakeData/groups';
+import { system } from './fakeData/system';
 
 export const fakeData = {
   me,
@@ -27,21 +28,23 @@ export const fakeData = {
   phAppTemplates,
   license,
   files,
+  system,
 };
 
-export const schema =  {
-  me: {type: 'object'},
-  secrets: {type: 'array', items: {type: 'object'}},
-  groups: {type: 'array', items: {type: 'object'}},
-  images: {type: 'array', items: {type: 'object'}},
-  phJobs: {type: 'array', items: {type: 'object'}},
-  phSchedules: {type: 'array', items: {type: 'object'}},
-  phDeployments: {type: 'array', items: {type: 'object'}},
-  mlflow: {type: 'object'},
-  models: {type: 'array', items: {type: 'object'}},
-  modelVersions: {type: 'array', items: {type: 'object'}},
-  phApplications: {type: 'array', items: {type: 'object'}},
-  phAppTemplates: {type: 'array', items: {type: 'object'}},
-  license: {type: 'object'},
-  files: {type: 'object'},
+export const schema = {
+  me: { type: 'object' },
+  secrets: { type: 'array', items: { type: 'object' } },
+  groups: { type: 'array', items: { type: 'object' } },
+  images: { type: 'array', items: { type: 'object' } },
+  phJobs: { type: 'array', items: { type: 'object' } },
+  phSchedules: { type: 'array', items: { type: 'object' } },
+  phDeployments: { type: 'array', items: { type: 'object' } },
+  mlflow: { type: 'object' },
+  models: { type: 'array', items: { type: 'object' } },
+  modelVersions: { type: 'array', items: { type: 'object' } },
+  phApplications: { type: 'array', items: { type: 'object' } },
+  phAppTemplates: { type: 'array', items: { type: 'object' } },
+  license: { type: 'object' },
+  files: { type: 'object' },
+  system: { type: 'object' },
 };
