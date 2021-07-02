0baf2b8157837272648e495c20da87004d01858e
Timothy Lee <ctiml@infuseai.io>
2021-07-02 14:23:44 +0800

    Remove trailing whitespace
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- packages/graphql-server/src/resolvers/notebook.ts
+++ packages/graphql-server/src/resolvers/notebook.ts
@@ -3,7 +3,7 @@ import { Context } from './interface';
 export const notifyNotebookEvent = async (root, args, context: Context) => {
   const { notebookStartedAt, notebookGpu, notebookStatus, notebookDuration } = args.data;
   const { telemetry } = context;
-  
+
   telemetry.track('Notebook Stopped', {
     notebookStartedAt,
     notebookGpu,
