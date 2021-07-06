3d175e694fbc13a614c0f29427386945ce216ab2
Jie Peng <im@jiepeng.me>
2021-07-06 17:48:35 +0800

    refactor(model): class -> fn with hooks
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/ee/containers/modelDetail.tsx
+++ packages/client/src/ee/containers/modelDetail.tsx
@@ -1,225 +1,290 @@
 import * as React from 'react';
-import {Skeleton, Button, Table, Row, Col, Tag, Modal,Icon} from 'antd';
+import { Button, Table, Row, Col, Tag, Modal, Icon } from 'antd';
+import { SortOrder } from 'antd/lib/table';
 import Field from 'components/share/field';
-import {graphql} from 'react-apollo';
-import {compose} from 'recompose';
-import {Link, withRouter} from 'react-router-dom';
-import {RouteComponentProps} from 'react-router';
+import { graphql } from 'react-apollo';
+import { compose } from 'recompose';
+import { Link, useParams, useHistory, withRouter } from 'react-router-dom';
+import { withApollo } from 'react-apollo';
+
 import PageTitle from 'components/pageTitle';
 import PageBody from 'components/pageBody';
-import { GroupContextComponentProps, withGroupContext } from 'context/group';
 import Breadcrumbs from 'components/share/breadcrumb';
-import {QueryModel, QueryModelVersionDeploy} from 'queries/Model.graphql';
-import {formatTimestamp, compareTimestamp, openMLflowUI, buildModelURI} from 'ee/components/modelMngt/common';
-import {DeployDialog} from 'ee/components/modelMngt/deployDialog';
-import { ApolloClient } from 'apollo-client';
-import { withApollo } from 'react-apollo';
-import { errorHandler } from "utils/errorHandler";
+import { GroupContext, withGroupContext } from 'context/group';
+import { QueryModel, QueryModelVersionDeploy } from 'queries/Model.graphql';
+import {
+  formatTimestamp,
+  openMLflowUI,
+  buildModelURI,
+  compareTimestamp,
+} from 'ee/components/modelMngt/common';
+import { DeployDialog } from 'ee/components/modelMngt/deployDialog';
+import { errorHandler } from 'utils/errorHandler';
 
-const PAGE_SIZE = 200;
-
-type Props = {
-  getModel: {
-    error?: any;
-    loading: boolean;
-    variables: {
-      where?;
+type ModelVersion = {
+  name: string;
+  version: string;
+  creationTimestamp: string;
+  lastUpdatedTimestamp: string;
+  deployedBy: {
+    id: string;
+    name: string;
+  }[];
+  run: {
+    info: {
+      runId: string;
+      experimentId: string;
+      status: string;
+      startTime: string;
+      endTime: string;
+      artifactUri: string;
+      lifecycleStage: string;
     };
-    refetch: Function;
-    mlflow?: any;
-    model?: any;
-    modelVersions?: any;
+    data: {
+      params: {
+        key: string;
+        value: string;
+      }[];
+      metricts: {
+        key: string;
+        value: string;
+        timestamp: string;
+        step: string;
+      }[];
+      tags: {
+        key: string;
+        value: string;
+      }[];
+    };
   };
-  client: ApolloClient<any>;
-} & RouteComponentProps & GroupContextComponentProps;
+};
 
-type State = {
-  deploy: {
-    modelVersion: Object;
-    deploymentRefs: Array<any>;
-  },
+interface Props {
+  client: any;
+  getModel: {
+    error?: Error;
+    loading: boolean;
+    mlflow: {
+      trackingUri: string;
+      uiUrl: string;
+    };
+    model: {
+      name: string;
+      creationTimestamp: string;
+      lastUpdatedTimestamp: string;
+      description: string;
+      latestVersions: {
+        name: string;
+        version: string;
+      }[];
+    };
+    refetch: () => Promise<void>;
+    modelVersions: ModelVersion[];
+  };
 }
 
-class ModelDetailContainer extends React.Component<Props, State> {
-  state: State = {
-    deploy: null,
-  };
+function ModelDetailContainer({ getModel, ...props }: Props) {
+  const [deploy, setDeploy] = React.useState(null);
+  const groupContext = React.useContext(GroupContext);
+  const { modelName } = useParams<{ modelName: string }>();
+  const history = useHistory();
 
-  private renderVersion = model => version => (
-    <Link to={`${model}/versions/${version}`}>
-      {`Version ${version}`}
-    </Link>
-  );
+  const decodeModelName = decodeURIComponent(modelName);
+  const breadcrumbs = [
+    {
+      key: 'list',
+      matcher: /\/models/,
+      title: 'Models',
+      link: '/models?page=1',
+    },
+    {
+      key: 'model',
+      matcher: /\/models/,
+      title: `Model: ${decodeModelName}`,
+      tips: 'View the model information and versioned model list.',
+      tipsLink:
+        'https://docs.primehub.io/docs/model-management#versioned-model-list',
+      onClick: () => {
+        getModel.refetch();
+      },
+    },
+  ];
 
-  private renderDeployedBy = deployedBy => {
-    const tags = [];
-    for (const element of deployedBy) {
-      tags.push(<Link to={`../deployments/${element.id}`}><Tag>{element.name}</Tag></Link>);
-    }
-    return (
-      <div>
-        {tags}
-      </div>
-    )
-  };
-
-  private handleDeploy = (modelVersion) => {
-    const {client, groupContext} = this.props;
-
+  function handleDeploy(modelVersion: ModelVersion) {
     if (!groupContext.enabledDeployment) {
       Modal.warn({
-        title: "Feature not available",
-        content: "Model Deployment is not enabled for this group. Please contact your administrator to enable it.",
+        title: 'Feature not available',
+        content:
+          'Model Deployment is not enabled for this group. Please contact your administrator to enable it.',
       });
       return;
     }
 
-    client.query<any>({
-      query: QueryModelVersionDeploy,
-      variables: {
-        groupId: groupContext.id
-      },
-      fetchPolicy: 'no-cache',
-    })
-    .then((result) => {
-      const deploy = {
-        modelVersion,
-        deploymentRefs: result.data.phDeployments
-      };
-      this.setState({deploy});
-    })
-    .catch(errorHandler);
+    props.client
+      .query({
+        query: QueryModelVersionDeploy,
+        variables: {
+          groupId: groupContext.id,
+        },
+        fetchPolicy: 'no-cache',
+      })
+      .then((result) => {
+        const deploy = {
+          modelVersion,
+          deploymentRefs: result.data.phDeployments,
+        };
+        setDeploy(deploy);
+      })
+      .catch(errorHandler);
   }
 
-  private handleDeployNew = (modelVersion) => {
-    const {history} = this.props;
-    const defaultValue = {modelURI: buildModelURI(modelVersion.name, modelVersion.version)};
-    history.push(`../deployments/create?defaultValue=${encodeURIComponent(JSON.stringify(defaultValue))}`);
-  }
+  function handleDeployNew(modelVersion) {
+    const defaultValue = {
+      modelURI: buildModelURI(modelVersion.name, modelVersion.version),
+    };
 
-  private handleDeployExisting = (modelVersion, deploymentRef) => {
-    const {history} = this.props;
-    const defaultValue = {modelURI: buildModelURI(modelVersion.name, modelVersion.version)};
-    history.push(`../deployments/${deploymentRef.id}/edit?defaultValue=${encodeURIComponent(JSON.stringify(defaultValue))}`);
-
-  }
-
-  private handleDeployCancel = () => {
-    this.setState({
-      deploy: null
-    });
-  }
-
-
-  render() {
-    const { groupContext, getModel, match} = this.props;
-    const { deploy } = this.state;
-    let {modelName} = match.params as any;
-    modelName = decodeURIComponent(modelName)
-
-    const {
-      mlflow,
-      model,
-      modelVersions,
-    } = getModel;
-
-    const breadcrumbs = [
-      {
-        key: 'list',
-        matcher: /\/models/,
-        title: 'Models',
-        link: '/models?page=1'
-      },
-      {
-        key: 'model',
-        matcher: /\/models/,
-        title: `Model: ${modelName}`,
-        tips: 'View the model information and versioned model list.',
-        tipsLink: 'https://docs.primehub.io/docs/model-management#versioned-model-list',
-        onClick: () => {getModel.refetch()}
-      }
-    ];
-
-    let pageBody;
-    if (getModel.error) {
-      pageBody = 'Cannot load model';
-    } else if (!model || !modelVersions) {
-      pageBody = <Skeleton />;
-    } else {
-      pageBody = this.renderModel(modelName, modelVersions, model, mlflow, getModel);
-    }
-
-    return (
-      <>
-        <PageTitle
-          breadcrumb={<Breadcrumbs pathList={breadcrumbs} />}
-          title={"Model Management"}
-        />
-        <PageBody>{pageBody}</PageBody>
-        {this.state.deploy ?
-          <DeployDialog
-            modelVersion={deploy.modelVersion}
-            deploymentRefs={deploy.deploymentRefs}
-            onDeployNew={this.handleDeployNew}
-            onDeployExisting={this.handleDeployExisting}
-            onCancel={this.handleDeployCancel}
-          /> : <></>
-        }
-      </>
+    history.push(
+      `../deployments/create?defaultValue=${encodeURIComponent(
+        JSON.stringify(defaultValue)
+      )}`
     );
   }
 
-  private renderModel(modelName: any, modelVersions: any, model: any, mlflow: any, getModel: { error?: any; loading: boolean; variables: { where?: any; }; refetch: Function; mlflow?: any; model?: any; modelVersions?: any; }) {
-    const columns: any = [{
+  function handleDeployExisting(modelVersion, deploymentRef) {
+    const defaultValue = {
+      modelURI: buildModelURI(modelVersion.name, modelVersion.version),
+    };
+    history.push(
+      `../deployments/${
+        deploymentRef.id
+      }/edit?defaultValue=${encodeURIComponent(JSON.stringify(defaultValue))}`
+    );
+  }
+
+  function handleDeployCancel() {
+    setDeploy(null);
+  }
+
+  const columns = [
+    {
+      key: 'Version',
       title: 'Version',
       dataIndex: 'version',
-      render: this.renderVersion(modelName),
-    }, {
+      render: (version: ModelVersion['version']) => (
+        <Link
+          to={`${decodeModelName}/versions/${version}`}
+        >{`Version ${version}`}</Link>
+      ),
+    },
+    {
+      key: 'Registered At',
       title: 'Registered At',
       dataIndex: 'creationTimestamp',
       render: formatTimestamp,
-      sorter: (a, b) => compareTimestamp(a.creationTimestamp, b.creationTimestamp),
-      defaultSortOrder: 'descend',
-    }, {
+      sorter: (a, b) =>
+        compareTimestamp(a.creationTimestamp, b.creationTimestamp),
+      defaultSortOrder: 'descend' as SortOrder,
+    },
+    {
+      key: 'Deployed By',
       title: 'Deployed By',
       dataIndex: 'deployedBy',
-      render: this.renderDeployedBy,
-    }, {
-      title: 'Action',
-      render: (text, modelVersion) => <Button onClick={() => {this.handleDeploy(modelVersion)}}>Deploy</Button>,
-    }];
-    const data = modelVersions;
+      render: (deployedBy: ModelVersion['deployedBy']) => {
+        const tags = deployedBy.map((deploy, id) => (
+          <Link key={id} to={`../deployments/${deploy.id}`}>
+            <Tag>{deploy.name}</Tag>
+          </Link>
+        ));
 
-    let pageBody = <>
-      <Row>
-        <Col span={19}>
-          <Field labelCol={4} valueCol={8} label='Created Time' value={formatTimestamp(model.creationTimestamp)} />
-          <Field labelCol={4} valueCol={8} label='Last Modified' value={formatTimestamp(model.lastUpdatedTimestamp)} />
-          <Field labelCol={4} valueCol={8} label='Description' value={model.description} />
-        </Col>
-        <Col span={5}>
-          <div style={{ display: 'flex', gap: '8px' }}>
+        return tags;
+      },
+    },
+    {
+      key: 'Action',
+      title: 'Action',
+      dataIndex: '',
+      render: (text, modelVersion: ModelVersion) => (
+        <Button
+          onClick={() => {
+            console.log(modelVersion);
+            handleDeploy(modelVersion);
+          }}
+        >
+          Deploy
+        </Button>
+      ),
+    },
+  ];
+
+  if (getModel.error) {
+    return <div>Can not not load model.</div>;
+  }
+
+  if (getModel.loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <PageTitle
+        breadcrumb={<Breadcrumbs pathList={breadcrumbs} />}
+        title={'Model Management'}
+      />
+      <PageBody>
+        <Row>
+          <Col span={19}>
+            <Field
+              labelCol={4}
+              valueCol={8}
+              label="Created Time"
+              value={formatTimestamp(getModel.model.creationTimestamp)}
+            />
+            <Field
+              labelCol={4}
+              valueCol={8}
+              label="Last Modified"
+              value={formatTimestamp(getModel.model.lastUpdatedTimestamp)}
+            />
+            <Field
+              labelCol={4}
+              valueCol={8}
+              label="Description"
+              value={formatTimestamp(getModel.model.description)}
+            />
+          </Col>
+          <Col span={5} style={{ display: 'flex', gap: '8px' }}>
             <Button>
               <Icon type="setting" />
               Columns
             </Button>
-            <Button onClick={() => {
-              openMLflowUI(mlflow, `/#/models/${encodeURIComponent(modelName)}`);
-            } }>
-                         MLflow UI
+            <Button
+              onClick={() => {
+                openMLflowUI(getModel.mlflow, `/#/models/${modelName}`);
+              }}
+            >
+              MLflow UI
             </Button>
-          </div>
-        </Col>
-      </Row>
-      <Table
-        style={{ paddingTop: 8 }}
-        dataSource={data}
-        columns={columns}
-        rowKey="name"
-        loading={getModel.loading} />
-    </>;
-    return pageBody;
-  }
+          </Col>
+        </Row>
+        <Table
+          style={{ paddingTop: 8 }}
+          dataSource={getModel.modelVersions}
+          columns={columns}
+          rowKey="name"
+          loading={getModel.loading}
+        />
+      </PageBody>
+      {deploy && (
+        <DeployDialog
+          modelVersion={deploy.modelVersion}
+          deploymentRefs={deploy.deploymentRefs}
+          onDeployNew={handleDeployNew}
+          onDeployExisting={handleDeployExisting}
+          onCancel={handleDeployCancel}
+        />
+      )}
+    </>
+  );
 }
 
 export default compose(
@@ -227,21 +292,21 @@ export default compose(
   withGroupContext,
   withApollo,
   graphql(QueryModel, {
-    options: (props: Props) => {
-      const {groupContext, match} = props;
-      let {modelName} = match.params as any;
-      modelName = decodeURIComponent(modelName)
+    options: (props: any) => {
+      const { groupContext, match } = props;
+      let { modelName } = match.params as any;
+      modelName = decodeURIComponent(modelName);
       const group = groupContext ? groupContext.name : undefined;
 
       return {
         variables: {
           group,
-          name: modelName
+          name: modelName,
         },
-        fetchPolicy: 'cache-and-network'
-      }
+        fetchPolicy: 'cache-and-network',
+      };
     },
     name: 'getModel',
     alias: 'withGetModel',
-  }),
-)(ModelDetailContainer)
+  })
+)(ModelDetailContainer);
