6047332ba38aede0e63eb935decb7a8f0b0913dc
Jie Peng <im@jiepeng.me>
2021-07-27 14:48:18 +0800

    test(secret): add test cases
    Signed-off-by: Jie Peng <im@jiepeng.me>

---
+++ packages/client/src/admin/Secrets/__tests__/Secrets.spec.tsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { MockedProvider } from 'react-apollo/test-utils';
+
+import { render, screen, waitFor } from 'test/test-utils';
+
+import { Secrets } from '../Secrets';
+import { GetSecrets } from '../secrets.graphql';
+import userEvent from '@testing-library/user-event';
+
+function setup() {
+  const mockRequests = [
+    {
+      request: {
+        query: GetSecrets,
+      },
+      result: {
+        data: {
+          secretsConnection: {
+            edges: [
+              {
+                cursor: 'gitsync-secret-csr-foobar',
+                node: {
+                  id: 'gitsync-secret-csr-foobar',
+                  name: 'csr-foobar',
+                  displayName: 'csr-foobarrr',
+                  type: 'opaque',
+                  __typename: 'Secret',
+                },
+                __typename: 'SecretEdge',
+              },
+              {
+                cursor: 'gitsync-secret-csr-foobar2',
+                node: {
+                  id: 'gitsync-secret-csr-foobar2',
+                  name: 'csr-foobar2',
+                  displayName: 'csr-foobarrr2',
+                  type: 'opaque',
+                  __typename: 'Secret',
+                },
+                __typename: 'SecretEdge',
+              },
+              {
+                cursor: 'gitsync-secret-csr-foobar3',
+                node: {
+                  id: 'gitsync-secret-csr-foobar3',
+                  name: 'csr-foobar3',
+                  displayName: 'csr-foobarrr3',
+                  type: 'opaque',
+                  __typename: 'Secret',
+                },
+                __typename: 'SecretEdge',
+              },
+            ],
+            __typename: 'SecretConnection',
+          },
+        },
+      },
+    },
+  ];
+
+  function TestProvider({ children }: { children: React.ReactNode }) {
+    return <MemoryRouter>{children}</MemoryRouter>;
+  }
+
+  return {
+    TestProvider,
+    mockRequests,
+  };
+}
+
+describe('Secrets', () => {
+  it('should render secrts with error messages', async () => {
+    const { TestProvider } = setup();
+
+    render(
+      <TestProvider>
+        <MockedProvider mocks={[]}>
+          <Secrets />
+        </MockedProvider>
+      </TestProvider>
+    );
+
+    expect(
+      await screen.findByText('Failure to load secrets.')
+    ).toBeInTheDocument();
+  });
+
+  it('should render secrts with fetched data', async () => {
+    const { TestProvider, mockRequests } = setup();
+
+    render(
+      <TestProvider>
+        <MockedProvider mocks={mockRequests}>
+          <Secrets />
+        </MockedProvider>
+      </TestProvider>
+    );
+
+    expect(await screen.findByText('csr-foobar')).toBeInTheDocument();
+    expect(await screen.findByText('csr-foobar2')).toBeInTheDocument();
+    expect(await screen.findByText('csr-foobar3')).toBeInTheDocument();
+  });
+
+  it('should render secret with create a new secret modal', async () => {
+    const { TestProvider, mockRequests } = setup();
+
+    render(
+      <TestProvider>
+        <MockedProvider mocks={mockRequests}>
+          <Secrets />
+        </MockedProvider>
+      </TestProvider>
+    );
+
+    const addSecretButton = await screen.findByText('Add');
+    userEvent.click(addSecretButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Create A Secret')).toBeInTheDocument();
+    });
+  });
+});
