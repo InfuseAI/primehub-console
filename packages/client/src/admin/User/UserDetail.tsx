e571a739b69eae5daaea331066573d9865d85a93
Aaron Huang <aaroms9733@gmail.com>
2021-08-12 10:47:08 +0800

    Lint patched
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/admin/User/UserDetail.tsx
+++ packages/client/src/admin/User/UserDetail.tsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import {notification, Form, Switch, Skeleton, Tabs, Table, Input, Col, Layout, Button, Icon, Modal } from 'antd';
-import { withRouter, useParams, useLocation, useHistory } from 'react-router-dom';
+import {
+  notification,
+  Form,
+  Switch,
+  Skeleton,
+  Tabs,
+  Layout,
+  Icon,
+  Input,
+} from 'antd';
+import {
+  withRouter,
+  useParams,
+  useLocation,
+  useHistory,
+  Link,
+} from 'react-router-dom';
 import { get, isArray, sortBy } from 'lodash';
 import { RouteComponentProps } from 'react-router';
 import PageTitle from 'components/pageTitle';
 import PageBody from 'components/pageBody';
 import { compose } from 'recompose';
 import Breadcrumbs from 'components/share/breadcrumb';
-import queryString from 'querystring';
 import { graphql } from 'react-apollo';
 import InfuseButton from 'components/infuseButton';
-import PHTooltip from 'components/share/toolTip';
 import SendEmail from 'cms-components/customize-object-email_form';
 import ResetPassword from 'cms-components/customize-object-password_form';
 import CheckableInputNumber from 'cms-components/customize-number-checkbox';
@@ -24,35 +37,36 @@ const groupColumns = [
   {
     title: 'Name',
     dataIndex: 'name',
-    render: (text, record, cannerProps) => {
-      const onClick = () => {
-        // /group/record.id
-      }
-      return <a onClick={onClick}>{text}</a>;
-    }
-  }, {
+    // eslint-disable-next-line react/display-name
+    render: (text, record) => {
+      return <Link to={`../group/${record.id}`}>{text}</Link>;
+    },
+  },
+  {
     title: 'Display Name',
-    dataIndex: 'displayName'
-  }, {
+    dataIndex: 'displayName',
+  },
+  {
     title: 'CPU Quota',
     dataIndex: 'quotaCpu',
     render: text => {
       return text === null ? '∞' : text;
     },
     // @ts-ignore
-    visible: !modelDeploymentOnly
-  }, {
+    visible: !modelDeploymentOnly,
+  },
+  {
     title: 'GPU Quota',
     dataIndex: 'quotaGpu',
     render: text => {
       return text === null ? '∞' : text;
     },
     // @ts-ignore
-    visible: !modelDeploymentOnly
-  }
+    visible: !modelDeploymentOnly,
+  },
 ];
 
-const groupPickerColumns =  [
+const groupPickerColumns = [
   {
     title: 'Name',
     dataIndex: 'name',
@@ -66,17 +80,17 @@ const GroupsRelationTable = compose(
   graphql(UserGroups, {
     name: 'queryUserGroups',
     alias: 'withQueryUserGroups',
-  }),
+  })
 )((props: any) => {
-  const {onChange, value, queryUserGroups} = props;
+  const { onChange, value, queryUserGroups } = props;
   const userGroups = get(queryUserGroups, 'groups', []);
-  const relationRefetch = (newVariables) => {
+  const relationRefetch = newVariables => {
     const { refetch, variables } = queryUserGroups;
     refetch({
       ...variables,
-      ...newVariables
+      ...newVariables,
     });
-  }
+  };
 
   return (
     <CustomRelationTable
@@ -86,8 +100,8 @@ const GroupsRelationTable = compose(
       relationValue={userGroups}
       relation={{
         to: 'group',
-          type: 'toMany',
-          fields: ['name', 'displayName', 'quotaCpu', 'quotaGpu']
+        type: 'toMany',
+        fields: ['name', 'displayName', 'quotaCpu', 'quotaGpu'],
       }}
       uiParams={{
         columns: groupColumns,
@@ -100,29 +114,39 @@ const GroupsRelationTable = compose(
 
 function DetailPage(props: any) {
   const { form, queryUser, queryUserGroups } = props;
-  const { id } = useParams<{id: string}>();
+  const { id } = useParams<{ id: string }>();
   const location = useLocation();
   const history = useHistory();
-  const [connections, setConnections] = useState({connect: [], disconnect: []});
+  const [connections, setConnections] = useState({
+    connect: [],
+    disconnect: [],
+  });
   const user = get(queryUser, 'user', {});
   const userGroups = get(queryUserGroups, 'groups', []);
   const [relateGroups, setRelateGroups] = useState([]);
   // Effect for original user data
   useEffect(() => {
-    setRelateGroups(get(user, 'groups', []))
+    setRelateGroups(get(user, 'groups', []));
   }, [user]);
   // Effect when connection changed
   useEffect(() => {
     if (connections.connect.length > 0 || connections.disconnect.length > 0) {
-      const allGroups = userGroups.edges.map((edge) => edge.node);
-      const {connect, disconnect} = connections;
+      const allGroups = userGroups.edges.map(edge => edge.node);
+      const { connect, disconnect } = connections;
       const resultIds = [
-        ...relateGroups.filter((g) => !disconnect.map((d) => d.id).includes(g.id)).map((g) => g.id),
-        ...connect.map((c) => c.id)
-      ]
-      setRelateGroups(sortBy(allGroups.filter((g) => resultIds.includes(g.id)), [(o) => o.name.toLowerCase()]));
+        ...relateGroups
+          .filter(g => !disconnect.map(d => d.id).includes(g.id))
+          .map(g => g.id),
+        ...connect.map(c => c.id),
+      ];
+      setRelateGroups(
+        sortBy(
+          allGroups.filter(g => resultIds.includes(g.id)),
+          [o => o.name.toLowerCase()]
+        )
+      );
     }
-  }, [connections])
+  }, [connections]);
   const breadcrumbs = [
     {
       key: 'list',
@@ -137,15 +161,23 @@ function DetailPage(props: any) {
     },
   ];
 
-  const handleRelationConnection = (actions) => {
+  const handleRelationConnection = actions => {
     if (isArray(actions) && actions.length > 0) {
-      const disconnect = actions.filter((action) => action.type === 'disconnect').map((d) => { return {id: d.value.id}});
-      const connect = actions.filter((action) => action.type === 'connect').map((c) => { return {id: c.value.id}});
-      setConnections({disconnect, connect});
+      const disconnect = actions
+        .filter(action => action.type === 'disconnect')
+        .map(d => {
+          return { id: d.value.id };
+        });
+      const connect = actions
+        .filter(action => action.type === 'connect')
+        .map(c => {
+          return { id: c.value.id };
+        });
+      setConnections({ disconnect, connect });
     }
-  }
+  };
 
-  const onSubmit = (e) => {
+  const onSubmit = e => {
     const { updateUser } = props;
     e.preventDefault();
     form.validateFields(async (err, values) => {
@@ -154,9 +186,9 @@ function DetailPage(props: any) {
         variables: {
           payload: {
             ...values,
-            groups: connections
-          }
-        }
+            groups: connections,
+          },
+        },
       });
     });
   };
@@ -177,103 +209,108 @@ function DetailPage(props: any) {
         title={'Users'}
       />
       <PageBody>
-          <Tabs>
-            <TabPane key='info' tab='Basic Info'>
-              <Skeleton loading={queryUser.loading || queryUserGroups.loading}>
-                <Form onSubmit={onSubmit}>
-                  <Form.Item label={'Username'}>
-                    {form.getFieldDecorator('username', {
-                      rules: [
-                        {
-                          required: true,
-                          pattern: /^[a-z0-9][-a-z0-9_.@]*$/,
-                          message: `Only lower case alphanumeric characters, '-', '.', and underscores ("_") are allowed, and must start with a letter or numeric.`,
-                        },
-                      ],
-                      initialValue: get(user, 'username'),
-                    })(<Input disabled={true}/>)}
-                  </Form.Item>
-                  <Form.Item label={'Email'}>
-                    {form.getFieldDecorator('email', {
-                      rules: [
-                        {
-                          pattern: /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                          message: `Please fill a valid email address format.`
-                        },
-                      ],
-                      initialValue: get(user, 'email'),
-                    })(<Input />)}
-                  </Form.Item>
-                  <Form.Item label={'First Name'}>
-                    {form.getFieldDecorator('firstName', {
-                      initialValue: get(user, 'firstName'),
-                    })(<Input />)}
-                  </Form.Item>
-                  <Form.Item label={'Last Name'}>
-                    {form.getFieldDecorator('lastName', {
-                      initialValue: get(user, 'lastName'),
-                    })(<Input />)}
-                  </Form.Item>
-                  <Form.Item label={'Enabled'}>
-                    {form.getFieldDecorator('enabled', {
-                      initialValue: get(user, 'enabled', false),
-                      valuePropName: 'checked'
-                    })(
+        <Tabs>
+          <TabPane key='info' tab='Basic Info'>
+            <Skeleton loading={queryUser.loading || queryUserGroups.loading}>
+              <Form onSubmit={onSubmit}>
+                <Form.Item label={'Username'}>
+                  {form.getFieldDecorator('username', {
+                    rules: [
+                      {
+                        required: true,
+                        pattern: /^[a-z0-9][-a-z0-9_.@]*$/,
+                        message: `Only lower case alphanumeric characters, '-', '.', and underscores ("_") are allowed, and must start with a letter or numeric.`,
+                      },
+                    ],
+                    initialValue: get(user, 'username'),
+                  })(<Input disabled={true} />)}
+                </Form.Item>
+                <Form.Item label={'Email'}>
+                  {form.getFieldDecorator('email', {
+                    rules: [
+                      {
+                        pattern:
+                          /(([^<>()[]\\.,;:\s@"]+(\.[^<>()[]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: `Please fill a valid email address format.`,
+                      },
+                    ],
+                    initialValue: get(user, 'email'),
+                  })(<Input />)}
+                </Form.Item>
+                <Form.Item label={'First Name'}>
+                  {form.getFieldDecorator('firstName', {
+                    initialValue: get(user, 'firstName'),
+                  })(<Input />)}
+                </Form.Item>
+                <Form.Item label={'Last Name'}>
+                  {form.getFieldDecorator('lastName', {
+                    initialValue: get(user, 'lastName'),
+                  })(<Input />)}
+                </Form.Item>
+                <Form.Item label={'Enabled'}>
+                  {form.getFieldDecorator('enabled', {
+                    initialValue: get(user, 'enabled', false),
+                    valuePropName: 'checked',
+                  })(
                     <Switch
                       checkedChildren={<Icon type='check' />}
                       unCheckedChildren={<Icon type='close' />}
                     />
                   )}
-                  </Form.Item>
-                  <Form.Item label={'Is Admin'}>
-                    {form.getFieldDecorator('isAdmin', {
-                      initialValue: get(user, 'isAdmin', false),
-                      valuePropName: 'checked'
-                    })(
+                </Form.Item>
+                <Form.Item label={'Is Admin'}>
+                  {form.getFieldDecorator('isAdmin', {
+                    initialValue: get(user, 'isAdmin', false),
+                    valuePropName: 'checked',
+                  })(
                     <Switch
                       checkedChildren={<Icon type='check' />}
                       unCheckedChildren={<Icon type='close' />}
                     />
-                    )}
-                  </Form.Item>
-                  <Form.Item label={'Personal Volume Capacity'}>
-                    {form.getFieldDecorator('volumeCapacity', {
-                      initialValue: get(user, 'volumeCapacity', null),
-                      getValueFromEvent: (refId, action, val) => val
-                    })(
-                      <CheckableInputNumber
-                        uiParams={{unit: ' GB', step: 1, min: 1, precision: 0, disableText: 'use default value'}}
-                      />
-                    )}
-                  </Form.Item>
-                  <Form.Item label={'Groups'}>
-                    <GroupsRelationTable
-                      onChange={handleRelationConnection}
-                      value={relateGroups}
+                  )}
+                </Form.Item>
+                <Form.Item label={'Personal Volume Capacity'}>
+                  {form.getFieldDecorator('volumeCapacity', {
+                    initialValue: get(user, 'volumeCapacity', null),
+                    getValueFromEvent: (refId, action, val) => val,
+                  })(
+                    <CheckableInputNumber
+                      uiParams={{
+                        unit: ' GB',
+                        step: 1,
+                        min: 1,
+                        precision: 0,
+                        disableText: 'use default value',
+                      }}
                     />
-                  </Form.Item>
-                  <Form.Item style={{textAlign: 'right', marginTop: 12}}>
-                    <InfuseButton
-                      type='primary'
-                      htmlType='submit'
-                      style={{marginRight: 16}}
-                    >
-                      Update
-                    </InfuseButton>
-                    <InfuseButton onClick={onCancel}>
-                      Cancel
-                    </InfuseButton>
-                  </Form.Item>
-                </Form>
-              </Skeleton>
-            </TabPane>
-            <TabPane key='send-email' tab='Send Email'>
-              <SendEmail rootValue={{}} refId={{}} id={id}/>
-            </TabPane>
-            <TabPane key='rese-pwd' tab='Reset Password'>
-              <ResetPassword rootValue={{}} refId={{}} id={id}/>
-            </TabPane>
-          </Tabs>
+                  )}
+                </Form.Item>
+                <Form.Item label={'Groups'}>
+                  <GroupsRelationTable
+                    onChange={handleRelationConnection}
+                    value={relateGroups}
+                  />
+                </Form.Item>
+                <Form.Item style={{ textAlign: 'right', marginTop: 12 }}>
+                  <InfuseButton
+                    type='primary'
+                    htmlType='submit'
+                    style={{ marginRight: 16 }}
+                  >
+                    Update
+                  </InfuseButton>
+                  <InfuseButton onClick={onCancel}>Cancel</InfuseButton>
+                </Form.Item>
+              </Form>
+            </Skeleton>
+          </TabPane>
+          <TabPane key='send-email' tab='Send Email'>
+            <SendEmail rootValue={{}} refId={{}} id={id} />
+          </TabPane>
+          <TabPane key='rese-pwd' tab='Reset Password'>
+            <ResetPassword rootValue={{}} refId={{}} id={id} />
+          </TabPane>
+        </Tabs>
       </PageBody>
     </Layout>
   );
@@ -283,7 +320,7 @@ export const UserDetail = compose(
   Form.create(),
   withRouter,
   graphql(User, {
-    options: (props: RouteComponentProps<{id: string}>) => {
+    options: (props: RouteComponentProps<{ id: string }>) => {
       return {
         onError: errorHandler,
         variables: {
@@ -307,7 +344,7 @@ export const UserDetail = compose(
         },
       },
       onCompleted: (data: any) => {
-        const {history} = props;
+        const { history } = props;
         history.push({
           pathname: `../users_next`,
         });
@@ -317,13 +354,18 @@ export const UserDetail = compose(
           message: 'Success!',
           description: (
             <>
-              User {data.updateUser.username} updated.
-              Click <a onClick={() => history.push(`user_next/${data.updateUser.id}`)}>here</a> to view.
+              User {data.updateUser.username} updated. Click{' '}
+              <a
+                onClick={() => history.push(`user_next/${data.updateUser.id}`)}
+              >
+                here
+              </a>{' '}
+              to view.
             </>
-          )
+          ),
         });
       },
-      onError: errorHandler
+      onError: errorHandler,
     }),
   }),
   graphql(UserGroups, {
@@ -331,8 +373,8 @@ export const UserDetail = compose(
     alias: 'withQueryUserGroups',
     options: (props: any) => {
       return {
-        fetchPolicy: 'cache-and-network'
-      }
-    }
-  }),
+        fetchPolicy: 'cache-and-network',
+      };
+    },
+  })
 )(DetailPage);
--- packages/client/src/cms-components/customize-number-checkbox.tsx
+++ packages/client/src/cms-components/customize-number-checkbox.tsx
@@ -1,12 +1,16 @@
-import React, { PureComponent } from "react";
-import { InputNumber, Checkbox, Input } from "antd";
-import isNaN from "lodash/isNaN";
+import React, { PureComponent } from 'react';
+import { InputNumber, Checkbox, Input } from 'antd';
 import isNull from 'lodash/isNull';
 
-export default class CheckableInputNumber extends PureComponent {
-  constructor(props) {
+interface Props {
+  uiParams: any
+}
+
+export default class CheckableInputNumber extends PureComponent<Props> {
+  defaultValue: number;
+  constructor(props: Props) {
     super(props);
-    const {uiParams} = props;
+    const { uiParams } = props;
     this.defaultValue = 0;
     if ('min' in uiParams) {
       this.defaultValue = uiParams.min;
@@ -15,49 +19,61 @@ export default class CheckableInputNumber extends PureComponent {
     if ('defaultValue' in uiParams) {
       this.defaultValue = uiParams.defaultValue;
     }
-
   }
 
-  onChange = (val) => {
-    const {onChange, refId, uiParams: {min}} = this.props;
+  onChange = val => {
+    const {
+      onChange,
+      refId,
+      uiParams: { min },
+    } = this.props;
     if (val >= min) {
-      onChange(refId, "update", val);
+      onChange(refId, 'update', val);
     }
-  }
+  };
 
-  onCheck = (e) => {
-    const {checked} = e.target;
-    const {onChange, refId} = this.props;
-    onChange(refId, "update", checked ? this.defaultValue : null);
-  }
+  onCheck = e => {
+    const { checked } = e.target;
+    const { onChange, refId } = this.props;
+    onChange(refId, 'update', checked ? this.defaultValue : null);
+  };
 
   render() {
     const { value, uiParams, disabled } = this.props;
     return (
       <React.Fragment>
-        <Checkbox onChange={this.onCheck} disabled={disabled} checked={!isNull(value)} style={{marginRight: 8}}/>
-        {
-          isNull(value) ? (
-            <Input
-              disabled
-              value={uiParams.disableText || "unlimited"}
-              style={{width: 'auto'}}
-            />
-          ) : (
-            <InputNumber
-              style={{width: 'auto'}}
-              disabled={disabled}
-              min={uiParams && uiParams.min}
-              max={uiParams && uiParams.max}
-              step={uiParams && uiParams.step}
-              precision={uiParams && uiParams.precision}
-              formatter={value => `${value}${uiParams && uiParams.unit ? uiParams.unit : ''}`}
-              parser={uiParams && uiParams.parser ? uiParams && uiParams.parser : value => value.replace(/[^0-9\.]/g, '')}
-              value={value}
-              onChange={this.onChange}
-            />
-          )
-        }
+        <Checkbox
+          onChange={this.onCheck}
+          disabled={disabled}
+          checked={!isNull(value)}
+          style={{ marginRight: 8 }}
+        />
+        {isNull(value) ? (
+          <Input
+            disabled
+            value={uiParams.disableText || 'unlimited'}
+            style={{ width: 'auto' }}
+          />
+        ) : (
+          <InputNumber
+            style={{ width: 'auto' }}
+            disabled={disabled}
+            min={uiParams && uiParams.min}
+            max={uiParams && uiParams.max}
+            step={uiParams && uiParams.step}
+            precision={uiParams && uiParams.precision}
+            formatter={value =>
+              `${value}${uiParams && uiParams.unit ? uiParams.unit : ''}`
+            }
+            parser={
+              uiParams && uiParams.parser
+                ? uiParams && uiParams.parser
+                : value => value.replace(/[^0-9\.]/g, '')
+            }
+            value={value}
+            onChange={this.onChange}
+          />
+        )}
       </React.Fragment>
     );
   }
