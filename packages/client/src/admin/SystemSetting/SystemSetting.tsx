d33010efc17d103036e0e6349d5eabd36e70523e
Aaron Huang <aaroms9733@gmail.com>
2021-08-18 17:17:28 +0800

    Cp Patch for SystemSettings breadcrumbs
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/admin/SystemSetting/SystemSetting.tsx
+++ packages/client/src/admin/SystemSetting/SystemSetting.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import moment from 'moment';
-import { Link } from 'react-router-dom';
 import {
-  Breadcrumb,
   Icon,
   Layout,
   Card,
@@ -20,6 +18,8 @@ import { graphql } from 'react-apollo';
 import { compose } from 'recompose';
 import { Controller, useForm } from 'react-hook-form';
 import { timezones } from 'react-timezone';
+
+import Breadcrumbs from 'components/share/breadcrumb';
 
 import { TimeZone } from './Timezone';
 import { LicenseStatus, LicenseTag } from './LicenseStatus';
@@ -131,7 +131,6 @@ interface Props {
 }
 
 function _SystemSetting({ data, ...props }: Props) {
-  const [appPrefix, setAppPrefix] = React.useState('/');
   const [addImageButtonVisible, setAddImageButtonVisible] =
     React.useState(false);
   const [pasteImageModalVisible, setPasteImageModalVisible] =
@@ -216,12 +215,6 @@ function _SystemSetting({ data, ...props }: Props) {
     }
   }, [data, reset]);
 
-  React.useEffect(() => {
-    if (window?.APP_PREFIX) {
-      setAppPrefix(window.APP_PREFIX);
-    }
-  }, []);
-
   if (data.error) {
     return <div>Error</div>;
   }
@@ -246,14 +239,15 @@ function _SystemSetting({ data, ...props }: Props) {
             marginBottom: 24,
           }}
         >
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Link to={`${appPrefix}admin/group`}>
-                <Icon type="home" />
-              </Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>System Settings</Breadcrumb.Item>
-          </Breadcrumb>
+          <Breadcrumbs
+            pathList={[
+              {
+                key: 'system',
+                matcher: /\/system/,
+                title: 'System Settings',
+              },
+            ]}
+          />
         </div>
 
         <h2>System Settings</h2>
