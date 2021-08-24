5e6f421fede63ede40bb19be35a71ee73d01e344
Timothy Lee <ctiml@infuseai.io>
2021-08-24 12:49:25 +0800

    Return if the deployment has gone
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- packages/client/src/ee/containers/DeploymentListContainer/DeploymentCard.tsx
+++ packages/client/src/ee/containers/DeploymentListContainer/DeploymentCard.tsx
@@ -61,6 +61,9 @@ interface Props
 }
 
 export function DeploymentCard({ endpoint, ...props }: Props) {
+  if (!props.name) {
+    return (null);
+  }
   const [isLoading, setIsLoading] = React.useState(false);
   const previousStatus = usePrevious(props.status);
   const history = useHistory();
