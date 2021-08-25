ab7acd5734f05767e2f77e30ec59d3de991540c5
Jie Peng <im@jiepeng.me>
2021-08-25 11:47:33 +0800

    fix(breadcrumbs): adjust breadcrumb basename
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/components/share/breadcrumb.tsx
+++ packages/client/src/components/share/breadcrumb.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import {Breadcrumb, Icon, Tooltip} from 'antd';
-import {withRouter, Link} from 'react-router-dom';
-import {RouteComponentProps} from 'react-router';
-import {appPrefix} from 'utils/env';
-import {compose} from 'recompose';
-import {LightA} from 'components/share';
+import * as React from 'react';
 import styled from 'styled-components';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { Breadcrumb, Icon, Tooltip } from 'antd';
+import { appPrefix } from 'utils/env';
+import { LightA } from 'components/share';
 
 const StyledBreadcrumb = styled(Breadcrumb)`
   font-size: 14px;
@@ -16,74 +14,111 @@ export interface BreadcrumbItemSetup {
   matcher: RegExp;
   title: string;
   link?: string;
-  onClick?: any;
-  tips?: any;
-  tipsLink?: any;
+  onClick?: () => void;
+  tips?: string | React.ReactNode;
+  tipsLink?: string;
 }
 
-type Props = RouteComponentProps & {
+interface Props {
   pathList?: BreadcrumbItemSetup[];
-};
+}
 
-const parseBreadcrumb = (match: any, basename: string, items: BreadcrumbItemSetup[]) => {
+const parseBreadcrumb = (
+  match: any,
+  basename: string,
+  items: BreadcrumbItemSetup[]
+) => {
   const result = [];
+
   if (items) {
     items.forEach((item, index) => {
       const last = index === items.length - 1;
-      const {key, matcher, title, link, onClick, tips, tipsLink} = item;
-      const tipTitle = <span>{tips} {tipsLink ? <LightA target='_blank' href={tipsLink}>Learn more</LightA> : <></>}</span>;
+      const { key, matcher, title, link, onClick, tips, tipsLink } = item;
+
+      const tipTitle = (
+        <span>
+          {tips}{' '}
+          {tipsLink ? (
+            <LightA target='_blank' href={tipsLink}>
+              Learn more
+            </LightA>
+          ) : (
+            <></>
+          )}
+        </span>
+      );
+
       if (matcher && matcher.test(match.url)) {
-        result.push(<Breadcrumb.Item  key={key}>
-          <span style={{fontWeight: (last) ? 500 : 'initial', color: (last) ? 'rgba(0, 0, 0, 0.65)' : null}}>
-            {
-              !!link ? <Link style={{color: (last) ? 'rgba(0, 0, 0, 0.65)' : null}} to={`${basename}${link}`}>{title}</Link> :
-                !!onClick ? <a style={{color: (last) ? 'rgba(0, 0, 0, 0.65)' : null}} onClick={onClick}>{title}</a> :
+        const breadcrumb = (
+          <Breadcrumb.Item key={key}>
+            <span
+              style={{
+                fontWeight: last ? 500 : 'initial',
+                color: last ? 'rgba(0, 0, 0, 0.65)' : null,
+              }}
+            >
+              {link ? (
+                <Link
+                  style={{ color: last ? 'rgba(0, 0, 0, 0.65)' : null }}
+                  to={`${basename}${link}`}
+                >
+                  {title}
+                </Link>
+              ) : onClick ? (
+                <a
+                  style={{ color: last ? 'rgba(0, 0, 0, 0.65)' : null }}
+                  onClick={onClick}
+                >
+                  {title}
+                </a>
+              ) : (
                 title
-            }
-          </span>
-          {
-            !!tips ? (
+              )}
+            </span>
+            {tips && (
               <span>
                 <Tooltip title={tipTitle} placement='right'>
-                  <Icon type='question-circle' style={
-                    {
-                      marginLeft: 8,
-                    }
-                  }/>
+                  <Icon type='question-circle' style={{ marginLeft: 8 }} />
                 </Tooltip>
               </span>
-            ) : <></>
-          }
-        </Breadcrumb.Item>);
+            )}
+          </Breadcrumb.Item>
+        );
+
+        result.push(breadcrumb);
       }
     });
   }
   return result;
 };
 
-class Breadcrumbs extends React.Component<Props> {
-  render() {
-    const { match, pathList } = this.props;
-    const params = match.params as any;
-    const basename = params.groupName ? `${appPrefix}g/${params.groupName}` : `${appPrefix}`;
-    const breadcrumbItems = [
-      <Breadcrumb.Item key='home'>
-        {
-          params.groupName ?
-          <Link to={`${basename}`}><Icon type='home' /></Link> :
-          <a href={`${basename}`}><Icon type='home' /></a>
-        }
+function Breadcrumbs(props: Props) {
+  const { pathList } = props;
+  const match = useRouteMatch<{ groupName?: string }>();
 
-      </Breadcrumb.Item>,
-      ...parseBreadcrumb(match, basename, pathList)
-    ];
+  const isAdminPortal = match.path.startsWith('/admin');
+  let basename = '';
 
-    return (
-      <StyledBreadcrumb>
-        {breadcrumbItems}
-      </StyledBreadcrumb>
-    );
+  if (isAdminPortal) {
+    basename = `${appPrefix}admin`;
+  } else {
+    if (match.params.groupName) {
+      basename = `${appPrefix}g/${match.params.groupName}`;
+    } else {
+      basename = `${appPrefix}`;
+    }
   }
+
+  const breadcrumbItems = [
+    <Breadcrumb.Item key='home'>
+      <Link to={`${basename}`}>
+        <Icon type='home' />
+      </Link>
+    </Breadcrumb.Item>,
+    ...parseBreadcrumb(match, basename, pathList),
+  ];
+
+  return <StyledBreadcrumb>{breadcrumbItems}</StyledBreadcrumb>;
 }
 
-export default compose(withRouter)(Breadcrumbs);
+export default Breadcrumbs;
