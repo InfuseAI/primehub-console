b4ab95d0988d5e454e1012f67c81d25ee996c4d3
Ching Yi, Chan <qrtt1@infuseai.io>
2021-08-30 15:30:14 +0800

    Fix UI update
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- packages/client/src/containers/sharedFiles/notebookViewer.tsx
+++ packages/client/src/containers/sharedFiles/notebookViewer.tsx
@@ -6,6 +6,94 @@ import './nbviewer.css'
 
 import Markdown from 'react-markdown'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import { reject } from 'lodash';
+
+const loadingNotebook: string = `{
+  "cells": [
+   {
+    "cell_type": "markdown",
+    "metadata": {},
+    "source": [
+     "Loading ..."
+    ]
+   },
+   {
+    "cell_type": "code",
+    "execution_count": null,
+    "metadata": {},
+    "outputs": [],
+    "source": []
+   }
+  ],
+  "metadata": {
+   "kernelspec": {
+    "display_name": "Python 3",
+    "language": "python",
+    "name": "python3"
+   },
+   "language_info": {
+    "codemirror_mode": {
+     "name": "ipython",
+     "version": 3
+    },
+    "file_extension": ".py",
+    "mimetype": "text/x-python",
+    "name": "python",
+    "nbconvert_exporter": "python",
+    "pygments_lexer": "ipython3",
+    "version": "3.7.6"
+   }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 4
+ }`;
+
+const errorNotebook: string = `{
+  "cells": [
+   {
+    "cell_type": "markdown",
+    "metadata": {},
+    "source": [
+     "### Error"
+    ]
+   },
+   {
+    "cell_type": "markdown",
+    "metadata": {},
+    "source": [
+     "Reason: $REASON"
+    ]
+   },
+   {
+    "cell_type": "markdown",
+    "metadata": {},
+    "source": [
+     "Content: $CONTENT"
+    ]
+   }
+  ],
+  "metadata": {
+   "kernelspec": {
+    "display_name": "Python 3",
+    "language": "python",
+    "name": "python3"
+   },
+   "language_info": {
+    "codemirror_mode": {
+     "name": "ipython",
+     "version": 3
+    },
+    "file_extension": ".py",
+    "mimetype": "text/x-python",
+    "name": "python",
+    "nbconvert_exporter": "python",
+    "pygments_lexer": "ipython3",
+    "version": "3.7.6"
+   }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 4
+ }`;
 
 
 const MarkdownAdapter = (props) => {
@@ -13,7 +101,7 @@ const MarkdownAdapter = (props) => {
 }
 
 function NotebookViewer(props) {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState(loadingNotebook);
   const filesPattern = /\/preview\/(files.+)/;
   const appPrefix = props.appPrefix;
   let filePath = window.location.href.match(filesPattern)
@@ -27,31 +115,36 @@ function NotebookViewer(props) {
   const fullPath = `${window.location.origin}${appPrefix}${filePath[1]}`;
   useEffect(() => {
     fetch(fullPath)
-      .then(res => res.text().then(text => setValue(text)))
-      .catch(e => setValue(`Error: cannot fetch content from ${fullPath}`));
+      .then(res => {
+        if (res.status === 200) {
+          return res;
+        }
+        setValue(errorNotebook.replace('$REASON', 'Not a valid ipynb').replace('$CONTENT', `cannot fetch content from ${fullPath}`));
+        reject(res);
+      })
+      .then(res => res.text().then((text) => {
+        try {
+          const data = JSON.parse(text);
+          if (data['nbformat'] === 4) {
+            setValue(text);
+          } else {
+            setValue(errorNotebook.replace('$REASON', 'Not a valid ipynb').replace('$CONTENT', text));
+          }
+        } catch (error) {
+          setValue(errorNotebook.replace('$REASON', `${error}`).replace('$CONTENT', text));
+        }
+      }))
+      .catch(error => {
+        setValue(errorNotebook.replace('$REASON', `${error}`).replace('$CONTENT', `cannot fetch content from ${fullPath}`));
+      });
   }, [props.location]);
 
-  if (value.startsWith('Error:')) {
-    return (
-      <div>{value}</div>
-    );
-  }
-
-  if (value.startsWith('{') && value.endsWith('}')) {
-    return (
-      <NbViewer
-        source={value}
-        markdown={MarkdownAdapter}
-        code={SyntaxHighlighter} />
-
-    );
-  }
-
   return (
-    <div>Loading ...</div>
+    <NbViewer
+      source={value}
+      markdown={MarkdownAdapter}
+      code={SyntaxHighlighter} />
   );
 }
-
-
 
 export default NotebookViewer;
