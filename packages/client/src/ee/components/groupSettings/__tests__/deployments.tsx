adfc4d414d306e1d7413ed2a30db0a56fff4152a
popcorny <celu@infuseai.io>
2021-09-03 09:05:16 +0800

    Fix wrong deployment settings in primehub deploy
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/client/src/ee/components/groupSettings/__tests__/deployments.tsx
+++ packages/client/src/ee/components/groupSettings/__tests__/deployments.tsx
@@ -10,6 +10,7 @@ function setup() {
     displayName: 'Test Group',
     admins: 'test',
     enabledDeployment: true,
+    maxDeploy: 5,
   };
 
   const user = {
@@ -34,6 +35,7 @@ function setup() {
 describe('GroupSettingsDeployments Component', () => {
   it('Should render group settings models', () => {
     const { group, user, currentUser } = setup();
+    global.__ENV__ = 'ee';
     render(
       <MockedProvider>
         <GroupSettingsDeployments currentUser={currentUser} groupContext={group} userContext={user} />
@@ -41,5 +43,18 @@ describe('GroupSettingsDeployments Component', () => {
     );
     expect(screen.queryByText('Model Deployment')).toBeInTheDocument();
     expect(screen.getByRole('switch')).toBeChecked();
+    expect(screen.queryByDisplayValue('5')).toBeInTheDocument();
+  });
+  it('Should not render model deployment switch when env modelDeploy', () => {
+    const { group, user, currentUser } = setup();
+    // @ts-ignore
+    global.__ENV__ = 'modelDeploy';
+    render(
+      <MockedProvider>
+        <GroupSettingsDeployments currentUser={currentUser} groupContext={group} userContext={user} />
+      </MockedProvider>
+    );
+    expect(screen.queryByText('Model Deployment')).not.toBeInTheDocument();
+    expect(screen.queryByDisplayValue('5')).toBeInTheDocument();
   });
 });
