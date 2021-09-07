c333105f769f331c7cad4e6b2949818388efde6e
Aaron Huang <aaroms9733@gmail.com>
2021-09-07 23:14:34 +0800

    Patch spec
    Signed-off-by: Aaron Huang <aaroms9733@gmail.com>

--- packages/client/src/cms-toolbar/__tests__/filter.spec.tsx
+++ packages/client/src/cms-toolbar/__tests__/filter.spec.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { IntlProvider } from 'react-intl';
 import { render, screen } from 'test/test-utils';
 import FilterGroup from '../filter';
@@ -28,5 +29,35 @@ describe('cms-toolbar > filter', () => {
 
     expect(screen.queryByText('Username')).toBeInTheDocument();
     expect(screen.queryByText('Email')).toBeInTheDocument();
+  });
+
+  it('FilterGroup submit should call changeFilter properly', () => {
+    const changeFilter = jest.fn();
+    const props = {
+      changeFilter,
+      where: {},
+      fields: [
+        {
+          type: 'text',
+          label: 'Username',
+          key: 'username',
+        },
+        {
+          type: 'text',
+          label: 'Email',
+          key: 'email',
+        },
+      ],
+    };
+    render(
+      <IntlProvider locale='en'>
+        <FilterGroup {...props} />
+      </IntlProvider>
+    );
+
+    const searchButton = screen.queryByTestId('search-button');
+    userEvent.click(searchButton);
+
+    expect(changeFilter).toHaveBeenCalledWith({});
   });
 });
