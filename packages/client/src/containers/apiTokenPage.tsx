27d13059c82b480ac14e2ec164fda970a9221781
popcorny <celu@infuseai.io>
2021-09-09 17:07:55 +0800

    Change to functional component
    Signed-off-by: popcorny <celu@infuseai.io>

--- packages/client/src/containers/apiTokenPage.tsx
+++ packages/client/src/containers/apiTokenPage.tsx
@@ -1,13 +1,23 @@
-import * as  React from 'react';
-import { Layout, Card, Button, Row, Input, message, Modal, Typography, Tag } from 'antd';
-import gql from 'graphql-tag';
+import React from 'react';
+import {
+  Layout,
+  Card,
+  Button,
+  Row,
+  Input,
+  message,
+  Modal,
+  Typography,
+} from 'antd';
+import { GetApiTokenCount, RevokeApiToken } from 'queries/ApiToken.graphql';
 import PageTitle from 'components/pageTitle';
 import { ApolloConsumer } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { errorHandler } from 'utils/errorHandler';
-import Breadcrumbs, {BreadcrumbItemSetup} from 'components/share/breadcrumb';
+import Breadcrumbs, { BreadcrumbItemSetup } from 'components/share/breadcrumb';
 import FileSaver from 'file-saver';
 import { GroupContext, GroupContextValue } from 'context/group';
+import { useClipboard } from 'hooks/useClipboard';
 
 const breadcrumbs: BreadcrumbItemSetup[] = [
   {
@@ -16,109 +26,70 @@ const breadcrumbs: BreadcrumbItemSetup[] = [
     title: 'API token',
     tips: 'A valid token is mandatory to allow 3rd-party using PrimeHub APIs.',
     tipsLink: 'https://docs.primehub.io/docs/tasks/api-token'
-  }
+  },
 ];
 
-type Props = {
-  client: ApolloClient<any>;
-}
+export default function ApiTokenPage() {
+  const graphqlEndpoint = window.absGraphqlEndpoint;
+  const apiToken = window.apiToken;
+  const example = `\nAPI_TOKEN="${apiToken ? apiToken : '<API TOKEN>'}"
 
-type State = {
-}
+curl -X POST \\
+  -H 'Content-Type: application/json' \\
+  -H "authorization: Bearer \${API_TOKEN}" \\
+  -d '{"query":"{me{id,username}}"}' \\
+  ${graphqlEndpoint}`;
 
-const GET_API_TOKEN_COUNT = gql`
-  query getApiTokenCount {
-    me {
-      apiTokenCount
-    }
-  }
-`;
+  const [statusCopyToken, copyToken] = useClipboard({
+    text: apiToken,
+    timeout: 2000,
+  });
+  const [, copyExample] = useClipboard({
+    text: example,
+    timeout: 2000,
+  });
 
-const REVOKE_API_TOKEN = gql`
-  mutation revokeApiToken {
-    revokeApiToken {
-      id
-    }
-  }
-`;
+  const handleRequestApiToken = async (client: ApolloClient<any>) => {
+    const requestApiTokenEndpoint = window.requestApiTokenEndpoint;
 
-class ApiTokenPage extends React.Component<Props, State> {
-  state = {
-    modal: false
-  }
+    const requestToken = async () => {
+      await client.mutate({ mutation: RevokeApiToken });
+      const backUrl = encodeURIComponent(window.location.pathname);
+      window.location.href = `${requestApiTokenEndpoint}?backUrl=${backUrl}`;
+    };
 
-  apiToken: any;
-  graphqlEndpoint: string;
-  requestApiTokenEndpoint: string;
-
-  refExample: React.RefObject<any> = React.createRef();
-  refToken: React.RefObject<any> = React.createRef();
-
-  constructor(props) {
-    super(props)
-
-    this.graphqlEndpoint = window.absGraphqlEndpoint;
-    this.apiToken = window.apiToken;
-    this.requestApiTokenEndpoint = window.requestApiTokenEndpoint;
-  }
-
-  handleRequestApiToken = () => {
-    const {client} = this.props;
-    client.query<any>({query: GET_API_TOKEN_COUNT, fetchPolicy: 'no-cache'})
-    .then((result) => {
-      if (result.data.me.apiTokenCount == 0) {
+    try {
+      const result = await client.query({
+        query: GetApiTokenCount,
+        fetchPolicy: 'no-cache',
+      });
+      if (result.data?.me?.apiTokenCount == 0) {
         // request token right away
-        this.requestToken();
+        await requestToken();
       } else {
         // show dialog if there is existing token
         Modal.confirm({
           title: 'Are you sure you want to request an API token?',
           content: 'Submitting a new request will revoke your existing token.',
-          onOk: () => {
-            this.requestToken();
+          onOk: async () => {
+            try {
+              await requestToken();
+            } catch (err) {
+              errorHandler(err);
+            }
           },
-          onCancel() {},
         });
       }
-    })
-    .catch(errorHandler);
-  }
-
-  requestToken = () => {
-    const {client} = this.props;
-    client.mutate({mutation: REVOKE_API_TOKEN})
-    .then(() => {
-      const backUrl = encodeURIComponent(window.location.pathname);
-      window.location.href = `${this.requestApiTokenEndpoint}?backUrl=${backUrl}`;
-    })
-    .catch(errorHandler);
-  }
-
-  copyToken = () => {
-    if (this.refToken && this.refToken.current) {
-      const input = this.refToken.current.input;
-      input.select();
-      document.execCommand('copy');
-      input.setSelectionRange(0, 0);
-      input.blur();
-      message.success('copied');
+    } catch (err) {
+      errorHandler(err);
     }
-  }
+  };
 
-  copyExample = () => {
-    if (this.refExample && this.refExample.current) {
-      this.refExample.current.textAreaRef.select();
-      document.execCommand('copy');
-      message.success('copied');
-      this.refExample.current.textAreaRef.blur();
-    }
-  }
-
-  downloadConfig = (groupContext: GroupContextValue) => {
+  const downloadConfig = (groupContext: GroupContextValue) => {
     const { id, name, displayName } = groupContext;
     const config = {
-      endpoint: this.graphqlEndpoint,
-      'api-token': this.apiToken,
+      endpoint: graphqlEndpoint,
+      'api-token': apiToken,
       group: {
         id,
         name,
@@ -133,94 +104,91 @@ class ApiTokenPage extends React.Component<Props, State> {
     FileSaver.saveAs(blob, 'config.json');
   };
 
-  render = () => {
-    const example = `\nAPI_TOKEN="${this.apiToken ? this.apiToken : '<API TOKEN>'}"
-
-curl -X POST \\
-    -H 'Content-Type: application/json' \\
-    -H "authorization: Bearer \${API_TOKEN}" \\
-    -d '{"query":"{me{id,username}}"}' \\
-    ${this.graphqlEndpoint}`
-
-    const apiToken = this.apiToken;
-
-    return (
-      <Layout>
-        <PageTitle
-          breadcrumb={<Breadcrumbs pathList={breadcrumbs} />}
-          title={'API token'}
-        />
-        <Row style={{ margin: "16px 24px" }}>
-          <Card title="Token">
-            {apiToken && (
-              <>
-                <Input
+  return (
+    <Layout>
+      <PageTitle
+        breadcrumb={<Breadcrumbs pathList={breadcrumbs} />}
+        title={'API token'}
+      />
+      <Row style={{ margin: '16px 24px' }}>
+        <Card title='Token'>
+          {apiToken && (
+            <>
+              <Input
                 readOnly
                 style={{ marginBottom: 16 }}
-                defaultValue={this.apiToken}
-                  addonAfter={
-                    <a
-                      onClick={() => this.copyToken()}
-                      style={{ color: 'black' }}
-                    >
-                      Copy
-                    </a>
-                  }
-                ref={this.refToken}
-              />
-
-              <Row style={{ marginBottom: 16 }}>
-                Please save this token. You won't be able to access it again. You can also download the config file for <a target='_blank'href='https://github.com/infuseai/primehub-python-sdk'> PrimeHub CLI/SDK</a> and save it at <Typography.Text code>~/.primehub/config.json</Typography.Text>.
-              </Row>
-            </>)}
-            <Button type="primary" onClick={this.handleRequestApiToken} >Request API Token</Button>
-            {apiToken && (
-              <GroupContext.Consumer>
-                {groupContext => (
-                  <Button
-                    style={{ marginLeft: 8 }}
-                    onClick={() => this.downloadConfig(groupContext)}
+                defaultValue={apiToken}
+                addonAfter={
+                  <a
+                    onClick={() => {
+                      copyToken();
+                      message.success('copied');
+                    }}
+                    style={{ color: 'black' }}
                   >
-                    Download Config
-                  </Button>
-                )}
-              </GroupContext.Consumer>
-            )}
-          </Card>
-          <Card title="Example" style={{ margin: "16px 0" }}>
-            <Button icon="copy" onClick={() => this.copyExample()}
-              style={{
-                float: 'right',
-                top: 32,
-                marginTop: -32,
-                zIndex: 10,
-                position: 'relative',
-                color: '#ccc',
-                borderColor: '#ccc'
-              }}
-              type="ghost"
-            >
-            </Button>
-            <Input.TextArea
-              style={{
-                whiteSpace: 'nowrap',
-                background: 'black',
-                color: '#ddd',
-                fontFamily: 'monospace',
-              }}
-              ref={this.refExample}
-              rows={9}
-              value={example}
+                    {statusCopyToken === 'copied' ? 'Copied' : 'Copy'}
+                  </a>
+                }
             />
-          </Card>
-        </Row>
-      </Layout>
-    )
-  }
-}
 
-export default () => (
-  <ApolloConsumer>
-    {client => <ApiTokenPage client={client} />}
-  </ApolloConsumer>
-)
+            <Row style={{ marginBottom: 16 }}>
+              Please save this token. You won't be able to access it again. You can also download the config file for <a target='_blank'href='https://github.com/infuseai/primehub-python-sdk'> PrimeHub CLI/SDK</a> and save it at <Typography.Text code>~/.primehub/config.json</Typography.Text>.
+            </Row>
+          </>)}
+          <ApolloConsumer>
+            {client => (
+              <Button
+                type='primary'
+                onClick={() => handleRequestApiToken(client)}
+              >
+              Request API Token
+              </Button>
+            )}
+          </ApolloConsumer>
+
+          {apiToken && (
+            <GroupContext.Consumer>
+              {groupContext => (
+                <Button
+                  style={{ marginLeft: 8 }}
+                  onClick={() => downloadConfig(groupContext)}
+                >
+                  Download Config
+                </Button>
+              )}
+            </GroupContext.Consumer>
+          )}
+        </Card>
+        <Card title='Example' style={{ margin: '16px 0' }}>
+          <Button
+            icon='copy'
+            onClick={() => {
+              copyExample();
+              message.success('copied');
+            }}
+            style={{
+              float: 'right',
+              top: 32,
+              marginTop: -32,
+              zIndex: 10,
+              position: 'relative',
+              color: '#ccc',
+              borderColor: '#ccc',
+            }}
+            type='ghost'
+          ></Button>
+          <Input.TextArea
+            style={{
+              whiteSpace: 'nowrap',
+              background: 'black',
+              color: '#ddd',
+              fontFamily: 'monospace',
+            }}
+            rows={9}
+            value={example}
+          />
+        </Card>
+      </Row>
+    </Layout>
+  );
+}
