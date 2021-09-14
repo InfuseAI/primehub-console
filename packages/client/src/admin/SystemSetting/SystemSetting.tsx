a7175b7811cd895649ed771cb9c736a5fdec29df
Timothy Lee <ctiml@infuseai.io>
2021-09-14 10:33:56 +0800

    Add apps import UI
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>
    Co-authored-by: Ash Wu <hsatac@infuseai.io>

--- packages/client/src/admin/SystemSetting/SystemSetting.tsx
+++ packages/client/src/admin/SystemSetting/SystemSetting.tsx
@@ -21,6 +21,7 @@ import { compose } from 'recompose';
 import { Controller, useForm } from 'react-hook-form';
 import { timezones } from 'react-timezone';
 
+import { errorHandler } from 'utils/errorHandler';
 import Breadcrumbs from 'components/share/breadcrumb';
 
 import { TimeZone } from './Timezone';
@@ -28,6 +29,7 @@ import { LicenseStatus, LicenseTag } from './LicenseStatus';
 import {
   GetSystemSetting,
   UpdateSystemSetting,
+  ImportPhAppTemplateFromURL,
 } from './systemSettings.graphql';
 
 function CustomLabel({
@@ -51,6 +53,9 @@ function CustomLabel({
 }
 
 const EMAIL_REGEX = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const GITHUB_REGEX =
+  /^https:\/\/github.com\/([A-Za-z0-9._-]+\/[A-Za-z0-9._-]+)\/blob\/(.+)/;
+const GITHUB_REGEX_REPLACE_VALUE = 'https://raw.githubusercontent.com/$1/$2';
 
 const initialState = {
   systemName: '',
@@ -130,6 +135,11 @@ interface Props {
       payload: Omit<SystemInfo, 'license'>;
     };
   }) => Promise<void>;
+  importPhAppTemplateFromURL: ({
+    variables,
+  }: {
+    variables: { url: string };
+  }) => Promise<void>;
 }
 
 function _SystemSetting({ data, ...props }: Props) {
@@ -137,6 +147,7 @@ function _SystemSetting({ data, ...props }: Props) {
     React.useState(false);
   const [pasteImageModalVisible, setPasteImageModalVisible] =
     React.useState(false);
+  const importURL = React.useRef(null);
 
   const { control, formState, reset, watch, handleSubmit } = useForm({
     defaultValues: initialState,
@@ -736,6 +747,31 @@ function _SystemSetting({ data, ...props }: Props) {
               )}
             </div>
           </Card>
+
+          <Card title='Apps'>
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+              }}
+            >
+              <Input ref={importURL} data-testid='import-template-url' />
+              <Button
+                type='primary'
+                data-testid='import-template-button'
+                onClick={() => {
+                  let url = importURL.current.input.value;
+                  if (url) {
+                    url = url.replace(GITHUB_REGEX, GITHUB_REGEX_REPLACE_VALUE);
+                    props.importPhAppTemplateFromURL({ variables: { url } });
+                  }
+                }}
+                style={{ marginLeft: '16px' }}
+              >
+                Import
+              </Button>
+            </div>
+          </Card>
         </form>
 
         <div
@@ -783,6 +819,21 @@ export const SystemSetting = compose(
           description: 'Update failure, try again later.',
         });
       },
+    }),
+  }),
+  graphql(ImportPhAppTemplateFromURL, {
+    name: 'importPhAppTemplateFromURL',
+    options: () => ({
+      onCompleted: data => {
+        const name = get(data, 'importPhAppTemplateFromURL.metadata.name', '');
+        notification.success({
+          duration: 5,
+          placement: 'bottomRight',
+          message: 'Successfully imported!',
+          description: `PhAppTemplate '${name}' imported`,
+        });
+      },
+      onError: errorHandler,
     }),
   }),
   graphql(GetSystemSetting, {
