f54390a63f0c11a69d3aa112986ae5b130b21040
Jie Peng <im@jiepeng.me>
2021-10-04 16:54:06 +0800

    style(model): add tooltip to name field
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/ee/containers/modelList.tsx
+++ packages/client/src/ee/containers/modelList.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import {Skeleton, Input, Alert, Button, Table} from 'antd';
-import {graphql} from 'react-apollo';
-import {compose} from 'recompose';
-import {get, max} from 'lodash';
-import {Link, withRouter} from 'react-router-dom';
+import { Skeleton, Tooltip, Typography, Alert, Button, Table } from 'antd';
+import { graphql } from 'react-apollo';
+import { compose } from 'recompose';
+import { get, max } from 'lodash';
+import { Link, withRouter } from 'react-router-dom';
 import queryString from 'querystring';
-import {RouteComponentProps} from 'react-router';
+import { RouteComponentProps } from 'react-router';
 import PageTitle from 'components/pageTitle';
 import PageBody from 'components/pageBody';
 import InfuseButton from 'components/infuseButton';
 import { GroupContextComponentProps, withGroupContext } from 'context/group';
 import Breadcrumbs from 'components/share/breadcrumb';
-import {formatTimestamp, openMLflowUI} from 'ee/components/modelMngt/common';
-import {QueryModels} from 'queries/Model.graphql';
+import { formatTimestamp, openMLflowUI } from 'ee/components/modelMngt/common';
+import { QueryModels } from 'queries/Model.graphql';
 
 const PAGE_SIZE = 20;
 
@@ -27,20 +27,34 @@ type Props = {
     mlflow?: any;
     models?: any;
   };
-} & RouteComponentProps & GroupContextComponentProps;
+} & RouteComponentProps &
+  GroupContextComponentProps;
 
 type State = {
   value: string;
-}
+};
 
 class ModelListContainer extends React.Component<Props, State> {
-  private renderName = text => text ? (
-    <Link to={`models/${encodeURIComponent(text)}`}>
-      {text}
-    </Link>
-  ) : '-'
+  private renderName = text => {
+    if (!text) {
+      return '-';
+    }
 
-  private renderLatestVersion= (latestVersions, model) => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center', maxWidth: '300px' }}>
+        <Tooltip placement='top' title={text}>
+          <Typography.Paragraph
+            ellipsis={{ rows: 3 }}
+            style={{ marginBottom: 0 }}
+          >
+            <Link to={`models/${encodeURIComponent(text)}`}>{text}</Link>
+          </Typography.Paragraph>
+        </Tooltip>
+      </div>
+    );
+  };
+
+  private renderLatestVersion = (latestVersions, model) => {
     if (!latestVersions || !Array.isArray(latestVersions)) {
       return '-';
     }
@@ -53,20 +67,20 @@ class ModelListContainer extends React.Component<Props, State> {
 
     const latestVersion = max(latestVersions);
 
-    return <Link to={`models/${encodeURIComponent(model.name)}/versions/${latestVersion}`}>
-      {`Version ${latestVersion}`}
-    </Link>
-  }
+    return (
+      <Link
+        to={`models/${encodeURIComponent(
+          model.name
+        )}/versions/${latestVersion}`}
+      >
+        {`Version ${latestVersion}`}
+      </Link>
+    );
+  };
 
   render() {
-    const { groupContext, getModels} = this.props;
-    const {
-      error,
-      loading,
-      mlflow,
-      models,
-      refetch
-    } = getModels;
+    const { getModels } = this.props;
+    const { error, loading, mlflow, models, refetch } = getModels;
 
     const breadcrumbs = [
       {
@@ -75,29 +89,48 @@ class ModelListContainer extends React.Component<Props, State> {
         title: 'Models',
         tips: 'Users can manage and version models here.',
         tipsLink: 'https://docs.primehub.io/docs/model-management',
-        onClick: () => {refetch()}
-      }
+        onClick: () => {
+          refetch();
+        },
+      },
     ];
 
     let pageBody;
     if (error) {
-      if (get(error, 'graphQLErrors.0.extensions.code') === 'TRACKING_URI_NOT_FOUND') {
-        pageBody = <Alert
-          message="MLflow is not configured"
-          description={<span>MLflow settings are not configured yet. Please go to the group settings to configure it. <a href='https://docs.primehub.io/docs/model-configuration'>Learn more</a></span>}
-          type="warning"
-          showIcon/>;
+      if (
+        get(error, 'graphQLErrors.0.extensions.code') ===
+        'TRACKING_URI_NOT_FOUND'
+      ) {
+        pageBody = (
+          <Alert
+            message='MLflow is not configured'
+            description={
+              <span>
+                MLflow settings are not configured yet. Please go to the group
+                settings to configure it.{' '}
+                <a href='https://docs.primehub.io/docs/model-configuration'>
+                  Learn more
+                </a>
+              </span>
+            }
+            type='warning'
+            showIcon
+          />
+        );
       } else if (error.graphQLErrors) {
-        pageBody = <Alert
-          message="Tracking URI Not Reachable"
-          description={`The configured MLflow tracking URI is not reachable. Please check if the MLflow is well configured and the MLflow server is running correctly.`}
-          type="error"
-          showIcon/>;
+        pageBody = (
+          <Alert
+            message='Tracking URI Not Reachable'
+            description={`The configured MLflow tracking URI is not reachable. Please check if the MLflow is well configured and the MLflow server is running correctly.`}
+            type='error'
+            showIcon
+          />
+        );
       } else {
-        pageBody = 'error'
+        pageBody = 'error';
       }
     } else if (loading) {
-      pageBody = <Skeleton />
+      pageBody = <Skeleton />;
     } else if (!models) {
       refetch();
     } else {
@@ -108,7 +141,7 @@ class ModelListContainer extends React.Component<Props, State> {
       <>
         <PageTitle
           breadcrumb={<Breadcrumbs pathList={breadcrumbs} />}
-          title={"Model Management"}
+          title={'Model Management'}
         />
         <PageBody>{pageBody}</PageBody>
       </>
@@ -116,39 +149,49 @@ class ModelListContainer extends React.Component<Props, State> {
   }
 
   private renderModels(mlflow: any, models: any, loading: boolean) {
-    const columns = [{
-      title: 'Name',
-      dataIndex: 'name',
-      render: this.renderName,
-    }, {
-      title: 'Latest Version',
-      dataIndex: 'latestVersions',
-      render: this.renderLatestVersion,
-    }, {
-      title: 'Creation Time',
-      dataIndex: 'creationTimestamp',
-      render: formatTimestamp,
-    }, {
-      title: 'Updated Time',
-      dataIndex: 'lastUpdatedTimestamp',
-      render: formatTimestamp,
-    }];
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        render: this.renderName,
+      },
+      {
+        title: 'Latest Version',
+        dataIndex: 'latestVersions',
+        render: this.renderLatestVersion,
+      },
+      {
+        title: 'Creation Time',
+        dataIndex: 'creationTimestamp',
+        render: formatTimestamp,
+      },
+      {
+        title: 'Updated Time',
+        dataIndex: 'lastUpdatedTimestamp',
+        render: formatTimestamp,
+      },
+    ];
 
-    let pageBody = <>
-      <div style={{ textAlign: 'right' }}>
-        <Button onClick={() => {
-          openMLflowUI(mlflow, '/#/models');
-        } }>
-          MLflow UI
-        </Button>
-        <Table
-          style={{ paddingTop: 8 }}
-          dataSource={models}
-          columns={columns}
-          rowKey="name"
-          loading={loading} />
-      </div>
-    </>;
+    const pageBody = (
+      <>
+        <div style={{ textAlign: 'right' }}>
+          <Button
+            onClick={() => {
+              openMLflowUI(mlflow, '/#/models');
+            }}
+          >
+            MLflow UI
+          </Button>
+          <Table
+            style={{ paddingTop: 8 }}
+            dataSource={models}
+            columns={columns}
+            rowKey='name'
+            loading={loading}
+          />
+        </div>
+      </>
+    );
     return pageBody;
   }
 }
@@ -158,16 +201,16 @@ export default compose(
   withGroupContext,
   graphql(QueryModels, {
     options: (props: Props) => {
-      const {groupContext} = props;
+      const { groupContext } = props;
       const group = groupContext ? groupContext.name : undefined;
       return {
         variables: {
           group,
         },
-        fetchPolicy: 'cache-and-network'
-      }
+        fetchPolicy: 'cache-and-network',
+      };
     },
     name: 'getModels',
     alias: 'withGetModels',
-  }),
-)(ModelListContainer)
+  })
+)(ModelListContainer);
