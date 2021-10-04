2b5c814ecbe65f8035c06de029bb69a7efb6bba8
Jie Peng <im@jiepeng.me>
2021-10-04 13:23:15 +0800

    style(tasks): adjust task name overflow
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/components/landing/recentTasks.tsx
+++ packages/client/src/components/landing/recentTasks.tsx
@@ -1,27 +1,39 @@
 import * as React from 'react';
 import moment from 'moment';
-import {groupBy} from 'lodash';
-import {Tag, Divider, Typography, Layout, Row, Col, Card, notification, Button} from 'antd';
-import {Link} from 'react-router-dom';
-import {PhJobsConnection} from 'queries/PhJob.graphql';
-import {PhDeploymentsConnection} from 'queries/PhDeployment.graphql';
-import {withGroupContext, GroupContextComponentProps} from 'context/group';
+import { groupBy } from 'lodash';
+import { Tag, Typography, Tooltip, Layout } from 'antd';
+import { Link } from 'react-router-dom';
+import { PhJobsConnection } from 'queries/PhJob.graphql';
+import { PhDeploymentsConnection } from 'queries/PhDeployment.graphql';
+import { withGroupContext, GroupContextComponentProps } from 'context/group';
 import styled from 'styled-components';
-import {graphql} from 'react-apollo';
-import {compose} from 'recompose';
+import { graphql } from 'react-apollo';
+import { compose } from 'recompose';
 import type { Phase } from 'ee/components/job/phase';
 
-const {Title, Text} = Typography;
+const { Title, Text } = Typography;
 
 export const ThinTitle = styled(Title)`
   font-weight: 200 !important;
 `;
 
-export const GuideList = styled.li`
-  list-style: none;
-  padding-left: 1em;
-  > li {
-    margin-top: 1em;
+const TaskName = styled.div`
+  max-width: calc(100% - 80px);
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const GuideList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  width: 100%;
+  padding-left: 1rem;
+
+  & > li {
+    width: 100%;
+    list-style: none;
+    margin-top: 1rem;
   }
 `;
 
@@ -56,15 +68,18 @@ const JOB_PHASE_COLOR: Partial<Record<Phase, string>> = {
 
 class RecentTasks extends React.Component<Props> {
   getRecentPhJobs() {
-    const {getPhJobsConnection} = this.props;
+    const { getPhJobsConnection } = this.props;
     const phJobsConnection = getPhJobsConnection.phJobsConnection;
     return phJobsConnection ? phJobsConnection.edges.slice(0, 2) : [];
   }
 
   getRecentPhDeployments() {
-    const {getPhDeploymentsConnection} = this.props;
-    const phDeploymentsConnection = getPhDeploymentsConnection.phDeploymentsConnection;
-    return phDeploymentsConnection ? phDeploymentsConnection.edges.slice(0, 2) : [];
+    const { getPhDeploymentsConnection } = this.props;
+    const phDeploymentsConnection =
+      getPhDeploymentsConnection.phDeploymentsConnection;
+    return phDeploymentsConnection
+      ? phDeploymentsConnection.edges.slice(0, 2)
+      : [];
   }
 
   render() {
@@ -72,48 +87,78 @@ class RecentTasks extends React.Component<Props> {
     const recentPhJobs = modelDeploymentOnly ? [] : this.getRecentPhJobs();
     const recentPhDeployments = this.getRecentPhDeployments();
 
-    const recentTasks = recentPhJobs.map(j => {
-      return {
-        type: 'Job',
-        id: j.node.id,
-        link: `job/${j.node.id}`,
-        name: j.node.displayName,
-        status: j.node.phase,
-        time: moment(j.node.createTime),
+    const recentTasks = recentPhJobs
+      .map(j => {
+        return {
+          type: 'Job',
+          id: j.node.id,
+          link: `job/${j.node.id}`,
+          name: j.node.displayName,
+          status: j.node.phase,
+          time: moment(j.node.createTime),
           statusColor: JOB_PHASE_COLOR[j.node.phase],
-      };
-    }).concat(recentPhDeployments.map(d => {
-      return {
-        type: 'Deployment',
-        name: d.node.name,
-        id: d.node.id,
-        link: `deployments/${d.node.id}`,
-        status: d.node.status,
-        statusColor: d.node.status === 'Deployed' ? 'green' : 'red',
-        time: moment(d.node.lastUpdatedTime)
-      };
-    })).sort((item, next) => item.time.isAfter(next.time) ? -1 : 1);
+        };
+      })
+      .concat(
+        recentPhDeployments.map(d => {
+          return {
+            type: 'Deployment',
+            name: d.node.name,
+            id: d.node.id,
+            link: `deployments/${d.node.id}`,
+            status: d.node.status,
+            statusColor: d.node.status === 'Deployed' ? 'green' : 'red',
+            time: moment(d.node.lastUpdatedTime),
+          };
+        })
+      )
+      .sort((item, next) => (item.time.isAfter(next.time) ? -1 : 1));
 
-    const groupedTasks = groupBy(recentTasks, t => t.time.clone().startOf('minute').fromNow());
+    const groupedTasks = groupBy(recentTasks, t =>
+      t.time.clone().startOf('minute').fromNow()
+    );
     return (
       <>
         <ThinTitle level={2}>Recent</ThinTitle>
         <SubContent>
           {Object.keys(groupedTasks).map((k, i) => {
-            return (<>
-              <ThinTitle level={3} style={{marginTop: i > 0 ? '0.5em' : ''}}>{k}</ThinTitle>
-              <GuideList>
-                {groupedTasks[k].map(t => {
-                  return (
-                    <li>
-                      <Link to={t.link}>[{t.type}] {t.name}</Link> <Tag color={t.statusColor}>{t.status}</Tag>
-                      <br/>
-                      <Text type='secondary'>{t.time.isValid() ? t.time.format('YYYY-MM-DD HH:mm:ss') : ''}</Text>
-                    </li>
-                  );
-                })}
-              </GuideList>
-            </>);
+            return (
+              <div key={i}>
+                <ThinTitle
+                  level={3}
+                  style={{ marginTop: i > 0 ? '0.5em' : '' }}
+                >
+                  {k}
+                </ThinTitle>
+                <GuideList>
+                  {groupedTasks[k].map(t => {
+                    return (
+                      <li key={t.name}>
+                        <div style={{ display: 'flex', gap: '8px' }}>
+                          <Tooltip placement='top' title={t.name}>
+                            <TaskName>
+                              <Link to={t.link}>
+                                <span style={{ marginRight: '4px' }}>
+                                  [{t.type}]
+                                </span>
+                                {t.name}
+                              </Link>
+                            </TaskName>
+                          </Tooltip>
+                          <Tag color={t.statusColor}>{t.status}</Tag>
+                        </div>
+
+                        <Text type='secondary'>
+                          {t.time.isValid()
+                            ? t.time.format('YYYY-MM-DD HH:mm:ss')
+                            : ''}
+                        </Text>
+                      </li>
+                    );
+                  })}
+                </GuideList>
+              </div>
+            );
           })}
         </SubContent>
       </>
@@ -124,41 +169,41 @@ class RecentTasks extends React.Component<Props> {
 export default compose(
   withGroupContext,
   graphql(PhJobsConnection, {
-  options: (props: Props) => {
-    const {groupContext} = props;
-    const where = {} as any;
-    if (groupContext) {
-      where.groupId_in = [groupContext.id];
-    }
+    options: (props: Props) => {
+      const { groupContext } = props;
+      const where = {} as any;
+      if (groupContext) {
+        where.groupId_in = [groupContext.id];
+      }
 
-    return {
-      variables: {
-        where,
-        orderBy: {},
-        page: 1,
-      },
-      fetchPolicy: 'cache-and-network'
-    };
-  },
-  name: 'getPhJobsConnection',
-  alias: 'withGetPhJobsConnection',
+      return {
+        variables: {
+          where,
+          orderBy: {},
+          page: 1,
+        },
+        fetchPolicy: 'cache-and-network',
+      };
+    },
+    name: 'getPhJobsConnection',
+    alias: 'withGetPhJobsConnection',
   }),
   graphql(PhDeploymentsConnection, {
-  options: (props: Props) => {
-    const {groupContext} = props;
-    const where = {} as any;
-    if (groupContext) {
-      where.groupId_in = [groupContext.id];
-    }
+    options: (props: Props) => {
+      const { groupContext } = props;
+      const where = {} as any;
+      if (groupContext) {
+        where.groupId_in = [groupContext.id];
+      }
 
-    return {
-      variables: {
-        where,
-      },
-      fetchPolicy: 'cache-and-network'
-    };
-  },
-  name: 'getPhDeploymentsConnection',
-  alias: 'withGetPhDeploymentsConnection',
-  }),
+      return {
+        variables: {
+          where,
+        },
+        fetchPolicy: 'cache-and-network',
+      };
+    },
+    name: 'getPhDeploymentsConnection',
+    alias: 'withGetPhDeploymentsConnection',
+  })
 )(RecentTasks);
