56974746f4c6560856450661c6754920b07713a2
popcorny <celu@infuseai.io>
2021-10-15 12:21:39 +0800

    Add telemetry for model management
    Signed-off-by: popcorny <celu@infuseai.io>
    Co-authored-by: Timothy Lee <ctiml@infuseai.io>

--- packages/graphql-server/src/ee/utils/telemetryTraits.ts
+++ packages/graphql-server/src/ee/utils/telemetryTraits.ts
@@ -1,6 +1,8 @@
 import {TraitMiddleware} from '../../utils/telemetry';
 import CrdClient from '../../crdClient/crdClientImpl';
 import { Config } from '../../config';
+import { getModelsTelemetry } from '../resolvers/model';
+import KcAdminClient from 'keycloak-admin';
 
 const licenseStatusString = async (config: Config): Promise<string> => {
   const status = config.licenseStatus.toLowerCase();
@@ -17,20 +19,28 @@ const scheduleCount = async (crdClient: CrdClient): Promise<number> => {
 
 interface CreateEETraitMiddlewareParams {
   config: Config;
+  createKcAdminClient: () => KcAdminClient;
+  getAccessToken: () => Promise<string>;
   crdClient: CrdClient;
 }
 
 export const createEETraitMiddleware = (params: CreateEETraitMiddlewareParams): TraitMiddleware => {
-  const {config, crdClient} = params;
+  const {createKcAdminClient, getAccessToken, config, crdClient} = params;
 
   return async (traits, next) => {
+    const accessToken = await getAccessToken();
+    const kcAdminClient: KcAdminClient = createKcAdminClient();
+    kcAdminClient.setAccessToken(accessToken);
+
     const licenseStatus = await licenseStatusString(config);
     const deployments = await modelDploymentCount(crdClient);
     const schedules = await scheduleCount(crdClient);
+    const modelsTelemetry = await getModelsTelemetry(config, kcAdminClient);
     Object.assign(traits, {
       licenseStatus,
       deployments,
       schedules,
+      ...modelsTelemetry,
     });
     next();
   };
