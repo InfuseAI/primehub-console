663e262510f12451e9c3a92346f5cddba6d3c77f
Aaron Huang <aaroms9733@gmail.com>
2021-10-26 15:39:55 +0800

    Merge pull request #408 from InfuseAI/feature/sc-22095/collect-net-promoter-score
    Add SurveySparrow dialog

--- packages/client/src/components/share/tracker.tsx
+++ packages/client/src/components/share/tracker.tsx
@@ -1,7 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { useLocalStorage } from 'hooks/useLocalStorage';
-import { Modal, Button, Icon, notification } from 'antd';
+import { Modal, Button, Icon, notification, Spin } from 'antd';
+import { delay } from 'lodash';
 
 const CommunityList = props => {
   const { userTakeAction } = props;
@@ -39,16 +40,78 @@ const CommunityList = props => {
   );
 };
 
-const PageChangeTracker = props => {
-  if (primehubCE !== true) {
-    return <></>;
+const waitUntilElementExists = (selector, callback) => {
+  const el = document.querySelector(selector);
+  if (el) {
+    return callback(el);
   }
+  setTimeout(() => waitUntilElementExists(selector, callback), 500);
+};
 
+const initSurvey = () => {
+  (() => {
+    window.SS_WIDGET_TOKEN = 'tt-6da14e';
+    window.SS_ACCOUNT = 'infuseai.surveysparrow.com';
+    window.SS_SURVEY_NAME = 'PrimeHub';
+    const surveyLoading = document.getElementById('survey_loading');
+    if (!document.getElementById('ss-widget')) {
+      const launcher = () => {
+        launcher.update(arguments);
+      };
+      launcher.args = [];
+      launcher.update = e => {
+        launcher.args.push(e);
+      };
+      window.SparrowLauncher = launcher;
+      const s = document.getElementsByTagName('script');
+      const c = s[s.length - 1];
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = !0;
+      script.id = 'ss-widget';
+      script.src = [
+        'https://',
+        'infuseai.surveysparrow.com/widget/',
+        window.SS_WIDGET_TOKEN,
+      ].join('');
+      script.addEventListener('load', () => {
+        waitUntilElementExists('#ss_widget_frame', () => {
+          delay(() => surveyLoading.remove(), 500);
+        });
+      });
+      c.parentNode.insertBefore(script, c);
+    }
+  })();
+};
+
+const NPSSurvey = () => {
+  useEffect(() => {
+    initSurvey();
+  }, []);
+  return (
+    <div style={{ height: '420px', overflow: 'hidden' }}>
+      <div id='ss_survey_widget'>
+        <div
+          id='survey_loading'
+          style={{
+            width: '100%',
+            height: '420px',
+            textAlign: 'center',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            overflow: 'hidden',
+          }}
+        >
+          <Spin tip='Loading'></Spin>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PageChangeTracker = () => {
   const analytics = window.analytics || null;
-  if (analytics == null) {
-    return <></>;
-  }
-
   const location = useLocation();
   const [visible, setVisible] = useState(false);
   const [promptType, setPromptType] = useState('modal');
@@ -76,7 +139,7 @@ const PageChangeTracker = props => {
       event.location = href;
     }
 
-    if (prompt >= 2) {
+    if (prompt >= 2 && !window.enableNPSSurvey) {
       setPromptType('notification');
       setVisible(true);
       event.prompt += 1;
@@ -94,10 +157,6 @@ const PageChangeTracker = props => {
     setEvent(event);
   };
 
-  useEffect(() => {
-    updatePageChange();
-  }, [location]);
-
   const userTakeAction = (shareTo: string) => {
     const s = event;
     s['collected'] = true;
@@ -108,6 +167,19 @@ const PageChangeTracker = props => {
   };
 
   const title = 'Thanks for using PrimeHub! Anything you want to share?';
+
+  useEffect(() => {
+    updatePageChange();
+  }, [location]);
+
+  if (primehubCE !== true && !window.enableNPSSurvey) {
+    return <></>;
+  }
+
+  if (analytics == null) {
+    return <></>;
+  }
+
   if (visible !== true || event.collected) {
     return <></>;
   }
@@ -118,6 +190,7 @@ const PageChangeTracker = props => {
         key='prompt-modal'
         title={title}
         visible={visible}
+        width={820}
         onCancel={() => {
           setVisible(false);
         }}
@@ -133,6 +206,7 @@ const PageChangeTracker = props => {
           </Button>,
         ]}
       >
+        {window.enableNPSSurvey ? <NPSSurvey /> : <></>}
         <CommunityList userTakeAction={userTakeAction} />
       </Modal>
     );
