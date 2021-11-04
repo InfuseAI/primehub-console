d262e7a2d8d31437d095d10dde641577251a28be
Aaron Huang <aaroms9733@gmail.com>
2021-11-04 18:34:54 +0800

    Merge pull request #419 from InfuseAI/bug/sc-21757/blank-screen-shown-when-switching-between
    Patch blank screen in setting page when switching group

--- packages/client/src/components/groupSettings/__tests__/members.spec.tsx
+++ packages/client/src/components/groupSettings/__tests__/members.spec.tsx
@@ -1,45 +1,17 @@
 import * as React from 'react';
 import GroupSettingsMembers from '../members';
-import { MockedProvider } from 'react-apollo/test-utils';
 import { render, screen } from 'test/test-utils';
-import { GetGroupUsers } from 'queries/Group.graphql';
-
-const AllTheProviders = ({ children }) => {
-  const groupUsersMock = {
-    request: {
-      query: GetGroupUsers,
-      variables: {
-        where: {
-          id: 'test-group',
-        },
-      },
-    },
-    result: {
-      data: {
-        group: {
-          id: 'test-group',
-          name: 'test-group',
-          admins: 'user1,user3',
-          users: [
-            { username: 'user1' },
-            { username: 'user2' },
-            { username: 'user3' },
-          ],
-        },
-      },
-    },
-  };
-
-  return (
-    <MockedProvider mocks={[groupUsersMock]} addTypename={false}>
-      {children}
-    </MockedProvider>
-  );
-};
 
 function setup() {
   const group = {
     id: 'test-group',
+    name: 'test-group',
+    admins: 'user1,user3',
+    users: [
+      { id: '1', username: 'user1' },
+      { id: '2', username: 'user2' },
+      { id: '3', username: 'user3' },
+    ],
   };
 
   return {
@@ -50,24 +22,31 @@ function setup() {
 describe('GroupSettingsMembers Component', () => {
   it('should render group settings members with loading status', () => {
     const { group } = setup();
-    render(<GroupSettingsMembers group={group} />, {
-      wrapper: AllTheProviders,
-    });
+    render(<GroupSettingsMembers />);
     expect(screen.getByText('loading...'));
   });
 
   it('Should render group settings members', async () => {
     const { group } = setup();
-    render(<GroupSettingsMembers group={group} />, { wrapper: AllTheProviders });
+    render(<GroupSettingsMembers group={group} />);
 
     expect(await screen.findByText('Group Admin')).toBeInTheDocument();
     expect(await screen.findByText('user1')).toBeInTheDocument();
     expect(await screen.findByText('user2')).toBeInTheDocument();
     expect(await screen.findByText('user3')).toBeInTheDocument();
 
-    const checkbox1 = screen.getByText('user1').closest('tr').querySelector('input');
-    const checkbox2 = screen.getByText('user2').closest('tr').querySelector('input');
-    const checkbox3 = screen.getByText('user3').closest('tr').querySelector('input');
+    const checkbox1 = screen
+      .getByText('user1')
+      .closest('tr')
+      .querySelector('input');
+    const checkbox2 = screen
+      .getByText('user2')
+      .closest('tr')
+      .querySelector('input');
+    const checkbox3 = screen
+      .getByText('user3')
+      .closest('tr')
+      .querySelector('input');
     expect(checkbox1.checked).toBe(true);
     expect(checkbox2.checked).toBe(false);
     expect(checkbox3.checked).toBe(true);
--- packages/client/src/components/groupSettings/members.tsx
+++ packages/client/src/components/groupSettings/members.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { get } from 'lodash';
-import { graphql } from 'react-apollo';
-import { compose } from 'recompose';
 import { Checkbox, Table } from 'antd';
 import { TruncateTableField } from 'utils/TruncateTableField';
-import { GetGroupUsers } from 'queries/Group.graphql';
 
 interface Props {
   group: any;
-  groupUsers: any;
 }
 
 class GroupSettingsMembers extends React.Component<Props> {
   render() {
-    const { group, groupUsers } = this.props;
+    const { group } = this.props;
 
-    if (!group || groupUsers.loading) {
+    if (!group) {
       return <div>loading...</div>;
     }
 
-    const admins = get(groupUsers, 'group.admins', []).split(',');
-    const users = get(groupUsers, 'group.users', []).map(user => {
+    const admins = get(group, 'admins', []).split(',');
+    const users = get(group, 'users', []).map(user => {
       return {
+        id: user.id,
         username: user.username,
         admin: admins.includes(user.username),
       };
@@ -45,26 +42,14 @@ class GroupSettingsMembers extends React.Component<Props> {
     ];
 
     return (
-      <Table dataSource={users} columns={columns} style={{ marginTop: 15 }} />
+      <Table
+        dataSource={users}
+        rowKey='id'
+        columns={columns}
+        style={{ marginTop: 15 }}
+      />
     );
   }
 }
 
-export default compose(
-  graphql(GetGroupUsers, {
-    options: (props: Props) => {
-      const { group } = props;
-      const where = {} as any;
-      if (group) {
-        where.id = group.id;
-      }
-
-      return {
-        variables: {
-          where,
-        },
-      };
-    },
-    name: 'groupUsers',
-  })
-)(GroupSettingsMembers);
+export default GroupSettingsMembers;
