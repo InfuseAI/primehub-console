6dc50cf92be00d78678f9408f362d87f4acc7d22
Timothy Lee <ctiml@infuseai.io>
2021-11-08 18:48:20 +0800

    Add submit for creating dataset
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>
    Co-authored-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- packages/client/src/containers/DatasetListContainer/DatasetList.tsx
+++ packages/client/src/containers/DatasetListContainer/DatasetList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import moment from 'moment';
-import { Table, Tag, Input, Alert, Pagination } from 'antd';
+import { notification, Table, Tag, Input, Alert, Pagination } from 'antd';
 import type { ColumnProps } from 'antd/lib/table';
 import { graphql } from 'react-apollo';
 import { compose } from 'recompose';
@@ -22,7 +22,7 @@ import { errorHandler } from 'utils/errorHandler';
 
 import { Dataset, DatasetConnection } from 'components/datasets/common';
 import { DatasetCreateForm } from 'components/datasets/createForm';
-import { GetDatasets } from './dataset.graphql';
+import { GetDatasets, CreateDatasetMutation } from './dataset.graphql';
 
 const DEFAULT_PAGE = 1;
 const DEFAULT_PAGE_SIZE = 10;
@@ -36,6 +36,13 @@ interface QueryVariables {
   page?: number;
 }
 
+interface InputVariables {
+  id: string;
+  name: string;
+  groupName: string;
+  tags: string[];
+}
+
 type Props = {
   groups: Array<{
     id: string;
@@ -47,6 +54,13 @@ type Props = {
     refetch: (variables?: QueryVariables) => void;
     datasetV2Connection: DatasetConnection;
   };
+  createDataset: ({
+    variables,
+  }: {
+    variables: {
+      payload: InputVariables;
+    };
+  }) => Promise<void>;
 } & RouteComponentProps &
   GroupContextComponentProps;
 
@@ -66,13 +80,29 @@ function CommonPageTitle() {
   return <PageTitle breadcrumb={<Breadcrumbs pathList={breadcrumbs} />} />;
 }
 
-function List({ groups, datasets, ...props }: Props) {
+function List({ groups, datasets, createDataset }: Props) {
   const groupContext = React.useContext(GroupContext);
   const [keyword, setKeyword] = React.useState('');
   const [modalVisible, setModalVisible] = React.useState(false);
 
-  function onSubmit(data) {
+  async function onSubmit(data) {
+    const { refetch, variables } = datasets;
+
     console.log(data);
+    await createDataset({
+      variables: {
+        payload: {
+          ...data,
+          groupName: groupContext.name,
+        },
+      },
+    });
+
+    refetch({
+      where: variables.where,
+      page: variables.page,
+    });
+
   }
 
   function onPageChanged(page) {
@@ -237,7 +267,11 @@ function List({ groups, datasets, ...props }: Props) {
             rowKey='id'
             pagination={false}
           />
-          <DatasetCreateForm visible={modalVisible} onClose={() => setModalVisible(false) } onSubmit={onSubmit} />
+          <DatasetCreateForm
+            visible={modalVisible}
+            onClose={() => setModalVisible(false)}
+            onSubmit={onSubmit}
+          />
         </div>
         <div
           style={{
@@ -276,5 +310,19 @@ export const DatasetList = compose(
       };
     },
     name: 'datasets',
+  }),
+  graphql(CreateDatasetMutation, {
+    options: {
+      onCompleted: (data: any) => {
+        const name = data.createDatasetV2.name;
+        notification.success({
+          message: `Dataset '${name}' has been created.`,
+          duration: 5,
+          placement: 'bottomRight',
+        });
+      },
+      onError: errorHandler,
+    },
+    name: 'createDataset',
   })
 )(List);
