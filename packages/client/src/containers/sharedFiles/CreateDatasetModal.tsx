97760c1ac8ba8f415b6ee1d88e8cb406ac1849e6
Jie Peng <im@jiepeng.me>
2021-12-30 10:39:39 +0800

    fix(shareFiles): appending slash at folder path
    Signed-off-by: Jie Peng <im@jiepeng.me>

--- packages/client/src/containers/sharedFiles/CreateDatasetModal.tsx
+++ packages/client/src/containers/sharedFiles/CreateDatasetModal.tsx
@@ -206,7 +206,7 @@ export function CreateDatasetModal({
                   }}
                 >
                   <b>Select Dataset</b>
-                  {!customFolderPath.isEditing && (
+                  {!customFolderPath.isEditing && customFolderPath.path !== '' && (
                     <Button
                       type='link'
                       onClick={() => {
@@ -245,6 +245,12 @@ export function CreateDatasetModal({
                       loadData={(node: AntTreeNode) =>
                         onLoadTreeNodes({ client, node })
                       }
+                      onChange={node => {
+                        setCustomFolderPath(prev => ({
+                          ...prev,
+                          path: node,
+                        }));
+                      }}
                     />
                   )
                 )}
@@ -285,7 +291,7 @@ export function CreateDatasetModal({
     form,
     files,
     groupName,
-    customFolderPath.isEditing,
+    customFolderPath,
     getFolderTree,
     onFileRemove,
   ]);
@@ -349,6 +355,7 @@ export function CreateDatasetModal({
           setUploadingToDataset(false);
           setUploadResult('idle');
           setTarget(null);
+          setCustomFolderPath({ isEditing: false, path: '' });
         }, 200);
       }
     };
@@ -374,7 +381,9 @@ export function CreateDatasetModal({
           : 'Cancel'
       }
       okButtonProps={{
-        disabled: files.length === 0 && uploadedResult !== 'success',
+        disabled:
+          (files.length === 0 && uploadedResult !== 'success') ||
+          customFolderPath.isEditing,
         style: {
           display: uploadingToDataset ? 'none' : 'inline-block',
           cursor:
@@ -427,15 +436,9 @@ export function CreateDatasetModal({
             setSteps(2);
             setFetching(true);
 
-            // Check the folder path is nested or not, if is a nested folder path,
-            // we need to exclude the root path and put remain path to `path`.
-            const isNested = values.id.indexOf('/') === -1 ? false : true;
-            const rootFolder = isNested
-              ? values.id.slice(0, values.id.indexOf('/'))
-              : values.id;
-            const nestedFolderPath = isNested
-              ? values.id.slice(values.id.indexOf('/'))
-              : '/';
+            // Splitting the folder to root folder and nested folder paths
+            const rootFolder = values.id.slice(0, values.id.indexOf('/'));
+            const nestedFolderPath = values.id.slice(values.id.indexOf('/'));
 
             try {
               const { endpoint } = await props.onCopyFiles({
