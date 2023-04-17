00df18dc93d94130ff4563f727168e9bacf8ba4a
Timothy Lee <ctiml@infuseai.io>
2023-04-17 18:51:36 +0800

    Add deploymentsUsage to group
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- packages/graphql-server/src/resolvers/group.ts
+++ packages/graphql-server/src/resolvers/group.ts
@@ -570,6 +570,18 @@ export const typeResolvers = {
       return [];
     }
   },
+  deploymentsUsage: async (parent, args, context: Context) => {
+    try {
+      const deployments = (await context.crdClient.phDeployments.list()) || [];
+      const deploymentsUsage = deployments.filter(deployment =>
+        deployment.spec.groupId === parent.id &&
+        deployment.spec.stop === false
+      ).length;
+      return deploymentsUsage;
+    } catch {
+      return null;
+    }
+  },
   resourceStatus: resourceStatusResolver.query,
   ...instanceTypeResolver.resolveInGroup(),
   ...datasetResolver.resolveInGroup(),
